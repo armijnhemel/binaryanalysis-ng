208b7c72ba774ca195423fead35371087b2bf926
Armijn Hemel <armijn@tjaldur.nl>
2018-07-26 23:15:38 +0200

    add more extensive and standard licensing notes

--- src/bangunpack.py
+++ src/bangunpack.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
 
-## Built in carvers/verifiers/unpackers for various formats.
+## Binary Analysis Next Generation (BANG!)
+##
+## This file is part of BANG.
+##
+## BANG is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Affero General Public License, version 3,
+## as published by the Free Software Foundation.
+##
+## BANG is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Affero General Public License for more details.
+##
+## You should have received a copy of the GNU Affero General Public License, version 3,
+## along with BANG.  If not, see <http://www.gnu.org/licenses/>
 ##
 ## Copyright 2018 - Armijn Hemel
 ## Licensed under the terms of the GNU Affero General Public License version 3
 ## SPDX-License-Identifier: AGPL-3.0-only
+
+## Built in carvers/verifiers/unpackers for various formats.
 ##
 ## Native Python unpackers/carvers for:
 ##
