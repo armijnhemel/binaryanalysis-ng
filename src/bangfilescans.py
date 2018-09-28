630bd82ffbf0984a15012d56bb2c8d8e08d1490e
Armijn Hemel <armijn@tjaldur.nl>
2018-09-28 21:25:02 +0200

    PEP-8 changes

--- src/bangfilescans.py
+++ src/bangfilescans.py
@@ -1,47 +1,48 @@
 #!/usr/bin/python3
 
-## Binary Analysis Next Generation (BANG!)
-##
-## This file is part of BANG.
-##
-## BANG is free software: you can redistribute it and/or modify
-## it under the terms of the GNU Affero General Public License,
-## version 3, as published by the Free Software Foundation.
-##
-## BANG is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU Affero General Public License for more details.
-##
-## You should have received a copy of the GNU Affero General Public
-## License, version 3, along with BANG. If not,
-## see <http://www.gnu.org/licenses/>
-##
-## Copyright 2018 - Armijn Hemel
-## Licensed under the terms of the GNU Affero General Public
-## License version 3
-## SPDX-License-Identifier: AGPL-3.0-only
+# Binary Analysis Next Generation (BANG!)
+#
+# This file is part of BANG.
+#
+# BANG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License,
+# version 3, as published by the Free Software Foundation.
+#
+# BANG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License, version 3, along with BANG. If not,
+# see <http://www.gnu.org/licenses/>
+#
+# Copyright 2018 - Armijn Hemel
+# Licensed under the terms of the GNU Affero General Public
+# License version 3
+# SPDX-License-Identifier: AGPL-3.0-only
 
-## This file has several methods for scanning files
-## Each file has the following parameters:
-##
-## * filename (pathlib.Path object)
-## * hashes (as a dict)
-## * database connection object (PostgreSQL)
-## * database cursor objec tPostgreSQL)
+# This file has several methods for scanning files
+# Each file has the following parameters:
+#
+# * filename (pathlib.Path object)
+# * hashes (as a dict)
+# * database connection object (PostgreSQL)
+# * database cursor objec tPostgreSQL)
+
 
 def knownfileNSRL(filename, hashresults, dbconn, dbcursor):
     '''A method to search a hash of a file in the NSRL database
     '''
-    ## results is (for now) a list
+    # results is (for now) a list
     results = []
 
-    if dbconn == None:
+    if dbconn is None:
         return results
 
-    ## first grab a *possible* filename from the NSRL database using
-    ## the SHA1 of the file. At the moment just one *possible* filename
-    ## is recorded in the database.
+    # first grab a *possible* filename from the NSRL database using
+    # the SHA1 of the file. At the moment just one *possible* filename
+    # is recorded in the database.
     dbcursor.execute("SELECT filename FROM nsrl_hash WHERE sha1=%s", (hashresults['sha1'],))
     filenameres = dbcursor.fetchall()
     dbconn.commit()
@@ -51,12 +52,12 @@ def knownfileNSRL(filename, hashresults, dbconn, dbcursor):
 
     manufacturercache = {}
 
-    ## get more results
+    # get more results
     dbcursor.execute("SELECT n.productname, n.productversion, n.applicationtype, n.manufacturercode FROM nsrl_product n, nsrl_entry m WHERE n.productcode = m.productcode AND m.sha1=%s;", (hashresults['sha1'],))
     productres = dbcursor.fetchall()
     dbconn.commit()
     for p in productres:
-        ## first create a result object
+        # first create a result object
         dbres = {}
         (productname, productversion, applicationtype, manufacturercode) = p
         if manufacturercode in manufacturercache:
@@ -64,10 +65,10 @@ def knownfileNSRL(filename, hashresults, dbconn, dbcursor):
         else:
             dbcursor.execute("SELECT manufacturername FROM nsrl_manufacturer WHERE manufacturercode=%s", (manufacturercode,))
             manufacturerres = dbcursor.fetchone()
-            if manufacturerres == None:
-                 ## this shouldn't happen
-                 dbconn.commit()
-                 return results
+            if manufacturerres is None:
+                # this shouldn't happen
+                dbconn.commit()
+                return results
             manufacturer = manufacturerres[0]
             manufacturercache[manufacturercode] = manufacturer
             dbconn.commit()
@@ -75,7 +76,7 @@ def knownfileNSRL(filename, hashresults, dbconn, dbcursor):
         dbres['productversion'] = productversion
         dbres['applicationtype'] = applicationtype
         dbres['manufacturer'] = manufacturer
-        ## add the result to the final list of results
+        # add the result to the final list of results
         results.append(dbres)
 
     return results
