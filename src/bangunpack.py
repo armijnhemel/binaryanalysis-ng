a99469668e8caa1b08ab665e800e99c5d6893ac3
Armijn Hemel <armijn@tjaldur.nl>
2019-01-17 17:06:15 +0100

    WAV: extra sanity check, there has to be at least one data chunk

--- src/bangunpack.py
+++ src/bangunpack.py
@@ -258,6 +258,12 @@ def unpackWAV(filename, offset, unpackdir, temporarydirectory):
                             b'note', b'plst', b'smpl', b'CDif', b'SAUR'])
     unpackres = unpackRIFF(filename, offset, unpackdir, validchunkfourcc, 'WAV', b'WAVE', filesize)
     if unpackres['status']:
+        # see if any data chunks were found at all
+        if not b'data' in unpackres['offsets']:
+            unpackingerror = {'offset': offset, 'fatal': False,
+                              'reason': 'no data chunk found'}
+            return {'status': False, 'error': unpackingerror}
+
         # see if any fmt chunks were found at all
         if not b'fmt ' in unpackres['offsets']:
             unpackingerror = {'offset': offset, 'fatal': False,
