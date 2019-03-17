cad847ad60117d31ed80942f05bd482f9a921390
Tim Hemel <tim@securesoftware.nl>
2019-03-17 16:31:00 +0100

    Fixes for pylint.

--- performance-test/bang-scanner/analyze-times.py
+++ performance-test/bang-scanner/analyze-times.py
@@ -6,7 +6,7 @@ import pathlib
 import csv
 
 
-def getExecutionInfo(scandir):
+def get_execution_info(scandir):
     # get bang.pickle in scandir
     scanresult = pickle.load(open(pathlib.Path(scandir) / 'bang.pickle', 'rb'))
     duration = scanresult['session']['duration']
@@ -14,6 +14,6 @@ def getExecutionInfo(scandir):
     return (scandir, checkfile, duration)
 
 if __name__ == "__main__":
-    r = [getExecutionInfo(arg) for arg in sys.argv[1:]]
+    r = [get_execution_info(arg) for arg in sys.argv[1:]]
     csv_writer = csv.writer(sys.stdout)
     csv_writer.writerows(r)
