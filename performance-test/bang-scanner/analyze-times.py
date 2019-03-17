ff81f9cf584e2f674b471d5d0059b2ad11548892
Tim Hemel <tim@securesoftware.nl>
2019-03-17 15:57:43 +0100

    scripts to run multiple scans and collect performance results into a CSV file.

---
+++ performance-test/bang-scanner/analyze-times.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import sys
+import pickle
+import pathlib
+import csv
+
+def getExecutionInfo(scandir):
+    # get bang.pickle in scandir
+    scanresult = pickle.load( open(pathlib.Path(scandir) / 'bang.pickle', 'rb' ) )
+    duration = scanresult['session']['duration']
+    checkfile = scanresult['session'].get('checkfile','unknown')
+    return (scandir, checkfile, duration)
+
+if __name__=="__main__":
+    r = [ getExecutionInfo(arg) for arg in sys.argv[1:] ]
+    csv_writer = csv.writer(sys.stdout)
+    csv_writer.writerows(r)
+
+
