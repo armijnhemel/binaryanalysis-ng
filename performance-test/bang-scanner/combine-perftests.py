3bd5091bb0fdd3c014894a84884b21e29126c640
Tim Hemel <tim@securesoftware.nl>
2019-03-27 14:27:53 +0100

    Test scripts to measure performance.

---
+++ performance-test/bang-scanner/combine-perftests.py
@@ -0,0 +1,16 @@
+
+import sys
+import csv
+import os
+
+rows = []
+
+for i in range(1,len(sys.argv)):
+    fn = os.path.join(sys.argv[i],'timings.json.csv')
+    reader = csv.reader(open(fn))
+    rows += list(reader)[1:]
+
+writer = csv.writer(sys.stdout)
+fieldnames=['testid','run','real','user','system','duration']
+writer.writerows([fieldnames]+rows)
+
