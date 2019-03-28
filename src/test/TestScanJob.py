28a78fa2b1df9e1ec49fe23213c394b3e7eb055c
Tim Hemel <tim@securesoftware.nl>
2019-03-29 00:21:52 +0100

    Testcase for bug when labeling css.

--- src/test/TestScanJob.py
+++ src/test/TestScanJob.py
@@ -48,7 +48,9 @@ class MockDBCursor:
     pass
 
 class TestScanJob(unittest.TestCase):
+
     def setUp(self):
+        self.testdata_dir = os.path.join(_scriptdir,'testdata')
         self.unpackdir = os.path.join(_scriptdir,'unpack')
         self.tmpdir = os.path.join(_scriptdir,'tmp')
         self.resultsdir = os.path.join(_scriptdir,'results')
@@ -77,19 +79,37 @@ class TestScanJob(unittest.TestCase):
         except FileNotFoundError:
             pass
         os.mkdir(dirname)
+
+    def _make_directory_in_unpackdir(self, dirname):
+        try:
+            os.makedirs(os.path.join(self.unpackdir, dirname))
+        except FileExistsError:
+            pass
+
     def _create_padding_file_in_directory(self):
         self.parent_dir = 'a'
-        os.makedirs(os.path.join(self.unpackdir, self.parent_dir))
+        self._make_directory_in_unpackdir(self.parent_dir)
         self.padding_file = os.path.join(self.parent_dir,'PADDING-0x00-0x01')
         f = open(os.path.join(self.unpackdir, self.padding_file), 'wb')
         f.write(b'\0' * 20)
         f.close()
+
+    def _create_css_file_in_directory(self):
+        self.parent_dir = 'a'
+        self._make_directory_in_unpackdir(self.parent_dir)
+        self.css_file = os.path.join(self.parent_dir,'cascade.css')
+        unpackedpath = os.path.join(self.unpackdir, self.css_file)
+        shutil.copy(os.path.join(self.testdata_dir, self.css_file),
+                unpackedpath)
+
     def _create_absolute_path_object(self,fn):
         return pathlib.Path(os.path.join(self.unpackdir, fn))
+
     def _create_fileresult_for_file(self,child,parent,labels):
         return FileResult(
                 self._create_absolute_path_object(child), child,
                 self._create_absolute_path_object(parent), parent, labels )
+
     def test_carved_padding_file_has_correct_labels(self):
         self._create_padding_file_in_directory()
         fileresult = self._create_fileresult_for_file(
@@ -121,6 +141,24 @@ class TestScanJob(unittest.TestCase):
             pass
         result = self.result_queue.get()
         self.assertSetEqual(result.labels,set(['binary','padding']))
+
+    def test_process_css_file_has_correct_labels(self):
+        # /home/tim/bang-test-scrap/bang-scan-jucli3nm/unpack/openwrt-18.06.1-brcm2708-bcm2710-rpi-3-ext4-sysupgrade.img.gz-gzip-1/openwrt-18.06.1-brcm2708-bcm2710-rpi-3-ext4-sysupgrade.img-ext2-1/www/luci-static/bootstrap/cascade.css
+        self._create_css_file_in_directory()
+        fileresult = self._create_fileresult_for_file(
+                self.css_file, self.parent_dir, set())
+        scanjob = ScanJob(fileresult)
+        self.scanfile_queue.put(scanjob)
+        try:
+            processfile(pathlib.Path(self.resultsdir),
+                    self.dbconn, self.dbcursor,
+                    self.scan_environment
+                    )
+        except QueueEmptyError:
+            pass
+        result = self.result_queue.get()
+        self.assertSetEqual(result.labels,set(['text','css']))
+
 
 
 if __name__=="__main__":
