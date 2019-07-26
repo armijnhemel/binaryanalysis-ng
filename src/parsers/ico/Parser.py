0461d63fdaeb043973aeb00b93fd76d8ed02ecc5
Tim Hemel <tim@securesoftware.nl>
2019-07-26 15:57:07 +0200

    First version of the ICO unpacker.

---
+++ src/parsers/ico/Parser.py
@@ -0,0 +1,54 @@
+import os
+from . import ico
+
+class IcoParser:
+    def __init__(self):
+        self.unpacked_size = 0
+        self.unpack_results = {}
+    def parse(self, fileresult, scan_environment, offset):
+        self.infile.seek(offset)
+        self.data = ico.Ico.from_io(self.infile)
+        self.unpacked_size = self.infile.tell() - offset
+        for i in self.data.images:
+            self.unpacked_size = max(self.unpacked_size, i.ofs_img + i.len_img)
+    def parse_and_unpack(self, fileresult, scan_environment, offset, unpack_dir):
+        try:
+            filename_full = scan_environment.unpack_path(fileresult.filename)
+            with filename_full.open('rb') as self.infile:
+                self.parse(fileresult, scan_environment, offset)
+                self.unpack_results = {
+                        'status': True,
+                        'length': self.unpacked_size
+                    }
+                self.set_metadata_and_labels(self.data)
+                files_and_labels = self.unpack(fileresult, scan_environment, offset, unpack_dir)
+                self.unpack_results['filesandlabels'] = files_and_labels
+                return self.unpack_results
+        except Exception as e:
+            # raise ParserException(*e.args)
+            unpacking_error = {
+                    'offset': offset + self.unpacked_size,
+                    'fatal' : False,
+                    'reason' : "{}: {}".format(e.__class__.__name__,str(e))
+                }
+            return { 'status' : False, 'error': unpacking_error }
+    def unpack(self, fileresult, scan_environment, offset, unpack_dir):
+        """extract any files from the input file"""
+        if offset != 0 or self.unpacked_size != fileresult.filesize:
+            outfile_rel = os.path.join(unpack_dir, "unpacked.ico")
+            outfile_full = scan_environment.unpack_path(outfile_rel)
+            os.makedirs(outfile_full.parent, exist_ok=True)
+            outfile = open(outfile_full, 'wb')
+            os.sendfile(outfile.fileno(), self.infile.fileno(), offset, self.unpacked_size)
+            outfile.close()
+            outlabels = self.unpack_results['labels'] + ['unpacked']
+            return [ (outfile_rel, outlabels) ]
+        else:
+            return []
+    def set_metadata_and_labels(self, metadata):
+        """sets metadata and labels for the unpackresults"""
+        self.unpack_results['labels'] = []
+        self.unpack_results['metadata'] = {}
+
+
+
---
+++ src/parsers/ico/Test.py
@@ -0,0 +1,29 @@
+import sys, os
+_scriptdir = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(_scriptdir, '..','..','test'))
+# import unittest
+from TestUtil import *
+
+from parsers.ico.Parser import IcoParser
+from ParserException import ParserException
+
+class TestIcoParser(TestBase):
+    def test_load_standard_ico_file(self):
+        rel_testfile = pathlib.Path('unpackers') / 'ico' / 'test.ico'
+        filename = pathlib.Path(self.testdata_dir) / rel_testfile
+        self._copy_file_from_testdata(rel_testfile)
+        fileresult = create_fileresult_for_path(self.unpackdir, rel_testfile)
+        filesize = fileresult.filesize
+        p = IcoParser()
+        # dummy data unpack dir
+        data_unpack_dir = (self.unpackdir / rel_testfile).parent
+        r = p.parse_and_unpack(fileresult, self.scan_environment, 0,
+                data_unpack_dir)
+        self.assertTrue(r['status'], r.get('error'))
+        self.assertEqual(r['length'], filesize)
+        self.assertEqual(r['filesandlabels'], [])
+        self.assertEqual(r['metadata']['width'], 64)
+
+if __name__ == '__main__':
+    unittest.main()
+
