0fa1e7a131ba7dc05bf56cbbf12d546ba93bf312
Tim Hemel <tim@securesoftware.nl>
2019-08-02 08:18:49 +0200

    Tests for UnpackParser conformance.

--- src/test/TestUnpackParser.py
+++ src/test/TestUnpackParser.py
@@ -2,6 +2,7 @@
 from TestUtil import *
 from UnpackParserException import UnpackParserException
 from UnpackParser import UnpackParser
+from bangsignatures import get_unpackers
 
 class InvalidUnpackParser(UnpackParser):
     pass
@@ -12,6 +13,15 @@ class TestFileResult(TestBase):
         with self.assertRaisesRegex(UnpackParserException, r"undefined parse method") as cm:
             p.parse()
 
+    def test_unpackparser_list_has_derived_classes_only(self):
+        self.assertNotIn(UnpackParser, get_unpackers())
+
+    def test_all_unpack_parsers_have_attributes(self):
+        for unpackparser in get_unpackers():
+            print(unpackparser)
+            self.assertIsNotNone(unpackparser.pretty_name)
+            self.assertIsNotNone(unpackparser.extensions)
+            self.assertIsNotNone(unpackparser.signatures)
 
 if __name__ == "__main__":
     unittest.main()
