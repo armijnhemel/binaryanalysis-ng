46ff7637013e5911e91f26a2b7698de9f213e3c7
Tim Hemel <tim@securesoftware.nl>
2019-08-05 14:24:06 +0200

    Change unit test to use UnpackParsers instead of unpackfunctions

--- src/test/TestUnpackResult.py
+++ src/test/TestUnpackResult.py
@@ -20,62 +20,32 @@ def get_unpackers_for_extension(ext):
 
 def get_unpackers_for_file(unpackername, f):
     ext = pathlib.Path(f).suffix
-    # ext = os.path.splitext(f)[1]
+
+    unpackers = { u for u in bangsignatures.get_unpackers()
+            if u.is_valid_extension(ext)
+            or u.pretty_name == unpackername }
+
+    for u in unpackers: yield u
     
-    for unpacker in get_unpackers_for_extension(ext):
-        yield unpacker
-    return
-
-    print('unpackername', unpackername)
-    try:
-        yield bangsignatures.signaturetofunction[unpackername]
-    except KeyError:
-        pass
-
-    for signature, prettyname in bangsignatures.signatureprettyprint.items():
-        if prettyname == unpackername:
-            try:
-                yield bangsignatures.signaturetofunction[signature]
-            except KeyError:
-                pass
-            try:
-                yield bangsignatures.textonlyfunctions[prettyname]
-            except KeyError:
-                pass
-    try:
-        prettyname = bangsignatures.extensionprettyprint[ext]
-        yield bangsignatures.textonlyfunctions[prettyname]
-    except KeyError:
-        pass
-
-    try:
-        yield bangsignatures.textonlyfunctions[unpackername]
-    except KeyError:
-        pass
-
 def is_prefix(pref, full):
     cp = os.path.commonprefix((pref, full))
     return cp == pref
 
 class TestUnpackResult(TestBase):
     def test_unpackdata_for_all_unpackers(self):
-        unpackers = bangsignatures.get_unpackers()
-        for f, u in self.walk_available_files_with_unpackers():
-            try:
-                del unpackers[u.__name__]
-            except KeyError as e:
-                pass
+        unpackers = set(bangsignatures.get_unpackers())
+        tested_unpackers = { u for f, u
+                in self.walk_available_files_with_unpackers() }
+        untested_unpackers = unpackers.difference(tested_unpackers)
         print("no tests for:")
-        print("\n".join([u for u in unpackers]))
-        # for all testdatafiles
-        self.assertEqual(unpackers, {})
+        print("\n".join([u.__name__ for u in untested_unpackers]))
+        self.assertEqual(untested_unpackers, set([]))
 
     def walk_available_files_with_unpackers(self):
         testfilesdir = self.testdata_dir / 'unpackers'
         for dirpath, dirnames, filenames in os.walk(testfilesdir):
             unpackername = os.path.basename(dirpath)
             for f in filenames:
-                # relativename = os.path.join(dirpath,f)[len(self.testdata_dir)+1:]
                 relativename = pathlib.Path(dirpath).relative_to(self.testdata_dir) / f
                 for unpacker in get_unpackers_for_file(unpackername, f):
                     yield str(relativename), unpacker
@@ -93,14 +63,11 @@ class TestUnpackResult(TestBase):
             ]
         for fn, unpackparser in sorted(set(self.walk_available_files_with_unpackers())):
             if fn in skipfiles:
-                # print('skip file', fn)
                 continue
-            # print('TestUnpackResult::before', os.getcwd())
             self._copy_file_from_testdata(fn)
             unpacker.make_data_unpack_directory(fn, unpackparser.__name__)
             fileresult = create_fileresult_for_path(self.unpackdir, pathlib.Path(fn))
-            unpackresult = unpackparser.parse_and_unpack(fileresult, self.scan_environment, 0, unpacker.get_data_unpack_directory())
-            # print('TestUnpackResult::after', os.getcwd())
+            unpackresult = unpackparser().parse_and_unpack(fileresult, self.scan_environment, 0, unpacker.get_data_unpack_directory())
 
             try:
                 # all paths in unpackresults are relative to unpackdir
@@ -129,7 +96,6 @@ class TestUnpackResult(TestBase):
             except KeyError as e:
                 pass
             finally:
-                # print("TestUnpackResult::remove data unpack directory")
                 unpacker.remove_data_unpack_directory_tree()
 
     def todo_test_unpackers_throw_exceptions(self):
@@ -140,7 +106,6 @@ class TestUnpackResult(TestBase):
         self._copy_file_from_testdata(fn, name)
         fileresult = create_fileresult_for_path(self.unpackdir, pathlib.Path(name))
         self.assertEqual(str(fileresult.filename), name)
-        # unpackresult = unpacker(fileresult, self.scan_environment, 0, self.unpackdir)
         for unpackername in sorted(unpackers.keys()):
             unpackparser = unpackers[unpackername]
             unpackresult = unpackparser.parse_and_unpack(fileresult, self.scan_environment, 0, self.unpackdir)
