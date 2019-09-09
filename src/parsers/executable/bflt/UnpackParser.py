08c462bf5e9f11f26858611e0d425830d77fc4c6
Tim Hemel <tim@securesoftware.nl>
2019-09-09 10:22:01 +0200

    Fix typo

--- src/parsers/executable/bflt/UnpackParser.py
+++ src/parsers/executable/bflt/UnpackParser.py
@@ -1,15 +1,15 @@
 
 import os
 from UnpackParser import UnpackParser
-from bangunpack import unpack_blft
+from bangunpack import unpack_bflt
 
-class BlftUnpackParser(UnpackParser):
+class BfltUnpackParser(UnpackParser):
     extensions = []
     signatures = [
         (0, b'bFLT')
     ]
-    pretty_name = 'blft'
+    pretty_name = 'bflt'
 
     def parse_and_unpack(self, fileresult, scan_environment, offset, unpack_dir):
-        return unpack_blft(fileresult, scan_environment, offset, unpack_dir)
+        return unpack_bflt(fileresult, scan_environment, offset, unpack_dir)
 
