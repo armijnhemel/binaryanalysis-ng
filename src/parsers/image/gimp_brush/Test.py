160a1d4f9e407690091db4bc537f22755474a305
Armijn Hemel <armijn@tjaldur.nl>
2019-09-12 11:54:05 +0200

    add test for gimp_brush

---
+++ src/parsers/image/gimp_brush/Test.py
@@ -0,0 +1,27 @@
+import sys, os
+from test.TestUtil import *
+
+from UnpackParserException import UnpackParserException
+from .UnpackParser import GimpBrushUnpackParser
+
+class TestGifUnpackParser(TestBase):
+    def test_load_standard_gbr_file(self):
+        rel_testfile = pathlib.Path('unpackers') / 'gimpbrush' / 'test.gbr'
+        filename = pathlib.Path(self.testdata_dir) / rel_testfile
+        self._copy_file_from_testdata(rel_testfile)
+        fileresult = create_fileresult_for_path(self.unpackdir, rel_testfile,
+                set())
+        filesize = fileresult.filesize
+        p = GimpBrushUnpackParser()
+        # dummy data unpack dir
+        data_unpack_dir = rel_testfile.parent / 'some_dir'
+        r = p.parse_and_unpack(fileresult, self.scan_environment, 0,
+                data_unpack_dir)
+        self.assertTrue(r['status'])
+        self.assertEqual(r['length'], filesize)
+        self.assertEqual(r['filesandlabels'], [])
+        self.assertEqual(r['metadata']['width'], 64)
+
+if __name__ == '__main__':
+    unittest.main()
+
