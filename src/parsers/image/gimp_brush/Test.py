7054cb2b5360ed5d75831921aa0e7b5d713e2526
Tim Hemel <tim@securesoftware.nl>
2019-09-14 00:30:24 +0200

    add test for failing test data: gimpbrush with offset.

--- src/parsers/image/gimp_brush/Test.py
+++ src/parsers/image/gimp_brush/Test.py
@@ -23,6 +23,24 @@ class TestGifUnpackParser(TestBase):
         self.assertEqual(r['filesandlabels'], [])
         self.assertEqual(r['metadata']['width'], 64)
 
+    def test_load_offset_gbr_file(self):
+        rel_testfile = pathlib.Path('unpackers') / 'gimpbrush' / 'test-prepend-random-data.gbr'
+        filename = pathlib.Path(self.testdata_dir) / rel_testfile
+        self._copy_file_from_testdata(rel_testfile)
+        fileresult = create_fileresult_for_path(self.unpackdir, rel_testfile,
+                set())
+        filesize = fileresult.filesize
+        data_unpack_dir = rel_testfile.parent / 'some_dir'
+        p = GimpBrushUnpackParser(fileresult, self.scan_environment,
+                data_unpack_dir, 128)
+        p.open()
+        r = p.parse_and_unpack()
+        p.close()
+        self.assertTrue(r['status'])
+        self.assertEqual(r['length'], filesize)
+        self.assertEqual(r['filesandlabels'], [])
+        self.assertEqual(r['metadata']['width'], 64)
+
 if __name__ == '__main__':
     unittest.main()
 
