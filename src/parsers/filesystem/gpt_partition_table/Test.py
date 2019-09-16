00e534b702a4b4bacde3e392ecfb3fef4579f304
Tim Hemel <tim@securesoftware.nl>
2019-09-16 17:41:16 +0200

    test to see if parsing MBR with gpt_partition_table parser fails.

--- src/parsers/filesystem/gpt_partition_table/Test.py
+++ src/parsers/filesystem/gpt_partition_table/Test.py
@@ -1,5 +1,6 @@
 import sys, os
 from test.TestUtil import *
+from UnpackParserException import UnpackParserException
 
 from .UnpackParser import GptPartitionTableUnpackParser
 
@@ -16,6 +17,19 @@ class TestGptPartitionTableUnpackParser(TestBase):
         self.assertTrue(r['status'])
         self.assertEqual(r['length'], self.get_testfile_size(rel_testfile))
         self.assertEqual(len(r['filesandlabels']), 4)
+
+    def test_load_mbr_partition_table(self):
+        rel_testfile = pathlib.Path('a') / \
+            'openwrt-18.06.1-brcm2708-bcm2710-rpi-3-ext4-sysupgrade.img'
+        data_unpack_dir = rel_testfile.parent / 'some_dir'
+        p = self.create_unpackparser_for_path(rel_testfile,
+                GptPartitionTableUnpackParser, 0,
+                data_unpack_dir = data_unpack_dir)
+        p.open()
+        with self.assertRaisesRegex(UnpackParserException, r".*") as cm:
+            r = p.parse_and_unpack()
+        p.close()
+
 
 if __name__ == '__main__':
     unittest.main()
