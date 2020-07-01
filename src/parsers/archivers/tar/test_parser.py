7be6dced15a22f784e2cae30605258898f28f90d
Tim Hemel <tim@securesoftware.nl>
2020-07-01 13:36:36 +0200

    make tar tests more pythonic with pytest.

--- src/parsers/archivers/tar/test_parser.py
+++ src/parsers/archivers/tar/test_parser.py
@@ -1,52 +1,48 @@
 import sys, os
-from test.TestUtil import *
+import pytest
+from test.util import *
 from UnpackParserException import UnpackParserException
 
 from .UnpackParser import TarUnpackParser
 
-class TestCpioUnpackParser(TestBase):
-    def test_load_tar_file(self):
-        rel_testfile = pathlib.Path('unpackers') / 'tar'/ 'test.tar'
-        self._copy_file_from_testdata(rel_testfile)
-        fileresult = create_fileresult_for_path(self.unpackdir, rel_testfile,
-                set())
-        data_unpack_dir = rel_testfile.parent / ('unpack-'+rel_testfile.name + "-1")
-        p = TarUnpackParser(fileresult, self.scan_environment, data_unpack_dir, 0)
-        p.open()
+def test_load_tar_file(scan_environment):
+    rel_testfile = pathlib.Path('unpackers') / 'tar'/ 'test.tar'
+    copy_testfile_to_environment(testdir_base / 'testdata', rel_testfile, scan_environment)
+    fr = fileresult(testdir_base / 'testdata', rel_testfile, set())
+    data_unpack_dir = rel_testfile.parent / ('unpack-'+rel_testfile.name + "-1")
+    p = TarUnpackParser(fr, scan_environment, data_unpack_dir, 0)
+    p.open()
+    r = p.parse_and_unpack()
+    p.close()
+    assert r['status']
+    extracted_fn = data_unpack_dir / 'test.sgi'
+    assert r['filesandlabels'] ==  [ (str(extracted_fn), []) ]
+
+
+def test_load_absolute_tar_file(scan_environment):
+    rel_testfile = pathlib.Path('unpackers') / 'tar'/ 'tar-abs.tar'
+    copy_testfile_to_environment(testdir_base / 'testdata', rel_testfile, scan_environment)
+    fr = fileresult(testdir_base / 'testdata', rel_testfile, set())
+    data_unpack_dir = rel_testfile.parent / ('unpack-'+rel_testfile.name + "-1")
+    p = TarUnpackParser(fr, scan_environment, data_unpack_dir, 0)
+    p.open()
+    r = p.parse_and_unpack()
+    p.close()
+    assert r['status']
+    extracted_fn = "/tmp/test.sgi"
+    assert r['filesandlabels'][0] == (str(extracted_fn), [])
+    # TODO: check where file is extracted
+
+
+def test_invalid_file_not_tar(scan_environment):
+    rel_testfile = pathlib.Path('unpackers') / 'gif'/ 'test.gif'
+    copy_testfile_to_environment(testdir_base / 'testdata', rel_testfile, scan_environment)
+    fr = fileresult(testdir_base / 'testdata', rel_testfile, set())
+    data_unpack_dir = rel_testfile.parent / ('unpack-'+rel_testfile.name + "-1")
+    p = TarUnpackParser(fr, scan_environment, data_unpack_dir, 0)
+    p.open()
+    with pytest.raises(UnpackParserException):
         r = p.parse_and_unpack()
-        p.close()
-        self.assertTrue(r['status'])
-        extracted_fn = data_unpack_dir / 'test.sgi'
-        self.assertEqual(r['filesandlabels'], [ (str(extracted_fn), []) ])
+    p.close()
 
-    def test_load_tar_file_absolute(self):
-        rel_testfile = pathlib.Path('unpackers') / 'tar'/ 'tar-abs.tar'
-        self._copy_file_from_testdata(rel_testfile)
-        fileresult = create_fileresult_for_path(self.unpackdir, rel_testfile,
-                set())
-        data_unpack_dir = rel_testfile.parent / ('unpack-'+rel_testfile.name + "-1")
-        p = TarUnpackParser(fileresult, self.scan_environment, data_unpack_dir, 0)
-        p.open()
-        r = p.parse_and_unpack()
-        p.close()
-        self.assertTrue(r['status'])
-        extracted_fn = "/tmp/test.sgi"
-        self.assertEqual(r['filesandlabels'][0], (str(extracted_fn), []))
-        # TODO: check where file is extracted
-
-
-    def test_invalid_file_not_tar(self):
-        rel_testfile = pathlib.Path('unpackers') / 'gif'/ 'test.gif'
-        self._copy_file_from_testdata(rel_testfile)
-        fileresult = create_fileresult_for_path(self.unpackdir, rel_testfile,
-                set())
-        data_unpack_dir = rel_testfile.parent / ('unpack-'+rel_testfile.name + "-1")
-        p = TarUnpackParser(fileresult, self.scan_environment, data_unpack_dir, 0)
-        p.open()
-        with self.assertRaises(UnpackParserException):
-            r = p.parse_and_unpack()
-        p.close()
-      
-    if __name__ == '__main__':
-        unittest.main()
 
