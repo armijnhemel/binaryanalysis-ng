2dfed25b54695222be6b1aae35a70701c6bc310e
Tim Hemel <tim@securesoftware.nl>
2020-07-02 09:05:40 +0200

    make tests more pythonic with pytest

--- src/parsers/filesystem/gpt_partition_table/test_parser.py
+++ src/parsers/filesystem/gpt_partition_table/test_parser.py
@@ -1,25 +1,20 @@
 import sys, os
-from test.TestUtil import *
+from test.util import *
 
 from .UnpackParser import GptPartitionTableUnpackParser
 
-class TestGptPartitionTableUnpackParser(TestBase):
-    def test_load_standard_file(self):
-        rel_testfile = pathlib.Path('a') / 'OPNsense-18.1.6-OpenSSL-vga-amd64.img'
-        self._copy_file_from_testdata(rel_testfile)
-        fileresult = create_fileresult_for_path(self.unpackdir, rel_testfile,
-                set())
-        filesize = fileresult.filesize
-        data_unpack_dir = rel_testfile.parent / 'some_dir'
-        p = GptPartitionTableUnpackParser(fileresult, self.scan_environment,
-                data_unpack_dir, 0)
-        p.open()
-        r = p.parse_and_unpack()
-        p.close()
-        self.assertTrue(r['status'])
-        self.assertEqual(r['length'], filesize)
-        self.assertEqual(len(r['filesandlabels']), 4)
+def test_load_standard_file(scan_environment):
+    rel_testfile = pathlib.Path('a') / 'OPNsense-18.1.6-OpenSSL-vga-amd64.img'
+    copy_testfile_to_environment(testdir_base / 'testdata', rel_testfile, scan_environment)
+    fr = fileresult(testdir_base / 'testdata', rel_testfile, set())
+    filesize = fr.filesize
+    data_unpack_dir = rel_testfile.parent / 'some_dir'
+    p = GptPartitionTableUnpackParser(fr, scan_environment, data_unpack_dir, 0)
+    p.open()
+    r = p.parse_and_unpack()
+    p.close()
+    assert r['status']
+    assert r['length'] == filesize
+    assert len(r['filesandlabels']) == 4
 
-if __name__ == '__main__':
-    unittest.main()
 
--- src/parsers/filesystem/mbr_partition_table/test_parser.py
+++ src/parsers/filesystem/mbr_partition_table/test_parser.py
@@ -1,42 +1,33 @@
 import sys, os
-from test.TestUtil import *
+from test.util import *
 from UnpackParserException import UnpackParserException
 
 from .UnpackParser import MbrPartitionTableUnpackParser
 
-class TestMbrPartitionTableUnpackParser(TestBase):
-    def test_load_standard_file(self):
-        rel_testfile = pathlib.Path('a') / \
-            'openwrt-18.06.1-brcm2708-bcm2710-rpi-3-ext4-sysupgrade.img'
-        self._copy_file_from_testdata(rel_testfile)
-        fileresult = create_fileresult_for_path(self.unpackdir, rel_testfile,
-                set())
-        filesize = fileresult.filesize
-        data_unpack_dir = rel_testfile.parent / 'some_dir'
-        p = MbrPartitionTableUnpackParser(fileresult, self.scan_environment,
-                data_unpack_dir, 0)
-        p.open()
+def test_load_standard_file(scan_environment):
+    rel_testfile = pathlib.Path('a') / \
+        'openwrt-18.06.1-brcm2708-bcm2710-rpi-3-ext4-sysupgrade.img'
+    copy_testfile_to_environment(testdir_base / 'testdata', rel_testfile, scan_environment)
+    fr = fileresult(testdir_base / 'testdata', rel_testfile, set())
+    filesize = fr.filesize
+    data_unpack_dir = rel_testfile.parent / 'some_dir'
+    p = MbrPartitionTableUnpackParser(fr, scan_environment, data_unpack_dir, 0)
+    p.open()
+    r = p.parse_and_unpack()
+    p.close()
+    assert r['status']
+    assert r['length'] == filesize
+    assert len(r['filesandlabels']) == 4
+
+def test_load_fat_partition(scan_environment):
+    rel_testfile = pathlib.Path('unpackers') / 'fat' / 'test.fat'
+    copy_testfile_to_environment(testdir_base / 'testdata', rel_testfile, scan_environment)
+    fr = fileresult(testdir_base / 'testdata', rel_testfile, set())
+    filesize = fr.filesize
+    data_unpack_dir = rel_testfile.parent / 'some_dir'
+    p = MbrPartitionTableUnpackParser(fr, scan_environment, data_unpack_dir, 0)
+    p.open()
+    with pytest.raises(UnpackParserException, match=r"no partitions") as cm:
         r = p.parse_and_unpack()
-        p.close()
-        self.assertTrue(r['status'])
-        self.assertEqual(r['length'], filesize)
-        self.assertEqual(len(r['filesandlabels']), 4)
-
-    def test_load_fat_partition(self):
-        rel_testfile = pathlib.Path('unpackers') / 'fat' / 'test.fat'
-        self._copy_file_from_testdata(rel_testfile)
-        fileresult = create_fileresult_for_path(self.unpackdir, rel_testfile,
-                set())
-        filesize = fileresult.filesize
-        data_unpack_dir = rel_testfile.parent / 'some_dir'
-        p = MbrPartitionTableUnpackParser(fileresult, self.scan_environment,
-                data_unpack_dir, 0)
-        p.open()
-        with self.assertRaisesRegex(UnpackParserException, r"no partitions") as cm:
-            r = p.parse_and_unpack()
-        p.close()
-
-
-if __name__ == '__main__':
-    unittest.main()
+    p.close()
 
