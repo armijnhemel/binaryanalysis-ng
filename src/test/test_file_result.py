7eca5dbeeb952f4581ee70be6ed72b1544a3d091
Tim Hemel <tim@securesoftware.nl>
2020-07-02 09:15:04 +0200

    change test to more pythonic pytest based test

--- src/test/test_file_result.py
+++ src/test/test_file_result.py
@@ -1,18 +1,14 @@
-from .TestUtil import *
+from .util import *
 
-class TestFileResult(TestBase):
-    def test_fileresult_has_correct_filenames(self):
-        relative_path = pathlib.Path("a/b/c.txt")
-        full_path = pathlib.Path(self.unpackdir) / relative_path
-        fr = create_fileresult_for_path(self.unpackdir, relative_path, set(), calculate_size=False)
-        d = fr.get()
-        self.assertEqual(fr.parent, str(relative_path.parent))
-        # self.assertEqual(fr.relpath, relative_path)
-        self.assertEqual(fr.filename, relative_path)
-        # self.assertEqual(d['fullfilename'], str(full_path))
-        self.assertEqual(d['filename'], str(relative_path))
-        self.assertEqual(d['parent'], str(relative_path.parent))
+def test_fileresult_has_correct_filenames(scan_environment):
+    relative_path = pathlib.Path("a/b/c.txt")
+    full_path = pathlib.Path(scan_environment.unpackdirectory) / relative_path
+    fr = fileresult(scan_environment.unpackdirectory, relative_path, set(), calculate_size=False)
+    d = fr.get()
+    assert fr.parent == str(relative_path.parent)
+    # assert fr.relpath == relative_path
+    assert fr.filename == relative_path
+    # assert d['fullfilename'] == str(full_path)
+    assert d['filename'] == str(relative_path)
+    assert d['parent'] == str(relative_path.parent)
 
-
-if __name__ == "__main__":
-    unittest.main()
