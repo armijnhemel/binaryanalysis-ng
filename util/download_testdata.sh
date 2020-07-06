d0830fc04e3183eaa33c17f7313bf88b74765b2a
Tim Hemel <tim@securesoftware.nl>
2020-07-06 15:55:42 +0200

    remove comment

--- util/download_testdata.sh
+++ util/download_testdata.sh
@@ -10,7 +10,6 @@ dlcurl ()
 	url=$1
 	fn=$2
 	mkdir -p $SCRIPT_DIR/../src/$TESTDATA_DIR/$(dirname "$fn") && \
-	# -k for insecure downloads
 	curl $url -o $SCRIPT_DIR/../src/$TESTDATA_DIR/"$fn"
 }
 
