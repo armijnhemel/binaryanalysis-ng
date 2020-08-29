53fc73a42e1284ed5a92b9b43ffef1dc9888707a
Tim Hemel <tim@securesoftware.nl>
2020-08-29 09:51:20 +0200

    Use pytest tempdir other than /tmp, which gets full easily. Remove old temporary test files before running a new test.

--- util/t.sh
+++ util/t.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+TEST_TMP_DIR=$(pwd)/tmp/bangtest
+
+rm -rf $TEST_TMP_DIR/*
+mkdir -p $TEST_TMP_DIR
+
+
 if [ -z "$*" ]
 then
-	docker container run -it --rm -u $(id -u):$(id -g) -v "$(pwd)":/src:z -v /tmp:/tmp:z -w /src bang bash
+	docker container run -it --rm -u $(id -u):$(id -g) -v "$(pwd)":/src:z -v "$TEST_TMP_DIR":/bangtmp:z -w /src bang bash
 else
-	docker container run -it --rm -u $(id -u):$(id -g) -v "$(pwd)":/src:z -v /tmp:/tmp:z -w /src bang "$@"
+	docker container run -it --rm -u $(id -u):$(id -g) -v "$(pwd)":/src:z -v "$TEST_TMP_DIR":/bangtmp:z -w /src bang "$@"
 fi
 
