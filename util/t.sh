dde6dd38caf2416e8c1b38a85f9c643823972a3e
Tim Hemel <tim@securesoftware.nl>
2020-08-29 09:16:24 +0200

    Fix problem with running tests from docker as normal user.

--- util/t.sh
+++ util/t.sh
@@ -2,8 +2,8 @@
 
 if [ -z "$*" ]
 then
-	docker container run -it --rm -v "$(pwd)":/src:z -w /src bang bash
+	docker container run -it --rm -u $(id -u):$(id -g) -v "$(pwd)":/src:z -v /tmp:/tmp:z -w /src bang bash
 else
-	docker container run -it --rm -v "$(pwd)":/src:z -w /src bang "$@"
+	docker container run -it --rm -u $(id -u):$(id -g) -v "$(pwd)":/src:z -v /tmp:/tmp:z -w /src bang "$@"
 fi
 
