16cdf13da366c30a67bcae7f0022a3d01686e740
Armijn Hemel <armijn@tjaldur.nl>
2021-02-02 20:57:22 +0100

    remove unnecessary if

--- maintenance/database/nsrlimporter.py
+++ maintenance/database/nsrlimporter.py
@@ -179,8 +179,6 @@ def main():
                             pass
                 offset += lastoffset
             decodedfile.close()
-
-    if args.decode:
         nsrlmfg = 'NSRLMfg.txt-translated'
         nsrlos = 'NSRLOS.txt-translated'
         nsrlprod = 'NSRLProd.txt-translated'
