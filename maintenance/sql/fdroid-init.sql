f253899021bcf15352fffaceb7a2ffdca21e27a5
Armijn Hemel <armijn@tjaldur.nl>
2021-02-16 16:11:28 +0100

    add (some) sql for F-Droid tables

---
+++ maintenance/sql/fdroid-init.sql
@@ -0,0 +1,3 @@
+create table if not exists fdroid_application(identifier text, source text, license text, primary key(identifier));
+create table if not exists fdroid_package(identifier text, version text, apkname text, srcpackage text, primary key(identifier, version));
+create table if not exists apk_contents(apkname text, filename text, sha256 text);
