37fda5e1bf008d0f0ad404175aa728090d6752f2
Armijn Hemel <armijn@tjaldur.nl>
2021-02-26 22:57:02 +0100

    use correct column for index

--- maintenance/sql/fdroid-init.sql
+++ maintenance/sql/fdroid-init.sql
@@ -2,4 +2,4 @@ CREATE TABLE IF NOT EXISTS fdroid_application(identifier text, source text, lice
 CREATE TABLE IF NOT EXISTS fdroid_package(identifier text, version text, apkname text, sha256 text, srcpackage text, PRIMARY KEY(identifier, version));
 CREATE TABLE IF NOT EXISTS apk_contents(apkname text, fullfilename text, filename text, sha256 text);
 CREATE INDEX apk_contents_sha256 ON apk_contents USING HASH (sha256);
-CREATE INDEX apk_contents_filename ON apk_contents USING HASH (sha256);
+CREATE INDEX apk_contents_filename ON apk_contents USING HASH (filename);
