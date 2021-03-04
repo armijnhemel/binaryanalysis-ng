431a5836cd1a57b4330377b1c5e4bebdf687b72b
Armijn Hemel <armijn@tjaldur.nl>
2021-03-04 22:06:51 +0100

    remove debugging output

--- src/parsers/game/doom_wad/UnpackParser.py
+++ src/parsers/game/doom_wad/UnpackParser.py
@@ -14,20 +14,16 @@ class DoomWadUnpackParser(UnpackParser):
     # http://web.archive.org/web/20090530112359/http://www.gamers.org/dhs/helpdocs/dmsp1666.html
     # chapter 2
     def parse(self):
-        print(self.infile.tell())
         try:
             self.data = doom_wad.DoomWad.from_io(self.infile)
         except (Exception, ValidationNotEqualError) as e:
-            print(e)
             raise UnpackParserException(e.args)
         check_condition(self.data.num_index_entries > 0, "no lumps defined")
 
     def calculate_unpacked_size(self):
-        print(self.offset, self.data.index_offset)
         self.unpacked_size = self.data.index_offset + self.data.num_index_entries * 16
         for i in self.data.index:
             self.unpacked_size = max(self.unpacked_size, i.offset + i.size)
-        print(self.unpacked_size)
 
     def set_metadata_and_labels(self):
         self.unpack_results['labels'] = ['doom', 'wad', 'resource']
