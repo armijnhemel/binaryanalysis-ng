a72fd1ee694c7edab0c5dad935e6483597f2ca6f
Armijn Hemel <armijn@tjaldur.nl>
2021-03-09 21:11:03 +0100

    midi: replace old parser with kaitai struct based parser

--- src/bangsignatures.py
+++ src/bangsignatures.py
@@ -300,7 +300,6 @@ signaturetofunction = {
     'pdf': bangmedia.unpack_pdf,
     'pack200': bangunpack.unpack_pack200,
     'zim': bangunpack.unpack_zim,
-    'midi': bangmedia.unpack_midi,
     'javakeystore': bangunpack.unpack_java_keystore,
     'xg3d': bangmedia.unpack_xg3d,
     'acdb': bangunpack.unpack_acdb,
