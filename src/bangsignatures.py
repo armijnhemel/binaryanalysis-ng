fb743d28c47ceeeeacbf34b6dc618972116c5b23
Armijn Hemel <armijn@tjaldur.nl>
2021-03-09 20:29:30 +0100

    rip out old doom wad parser

--- src/bangsignatures.py
+++ src/bangsignatures.py
@@ -325,7 +325,6 @@ signaturetofunction = {
     'romfs': bangfilesystems.unpack_romfs,
     'cramfs_le': bangfilesystems.unpack_cramfs,
     'cramfs_be': bangfilesystems.unpack_cramfs,
-    'doomwad': banggames.unpack_doom_wad,
     'ambarella': bangunpack.unpack_ambarella,
     'romfs_ambarella': bangunpack.unpack_romfs_ambarella,
     'bflt': bangunpack.unpack_bflt,
--- src/parsers/game/doom_wad/UnpackParser.py
+++ src/parsers/game/doom_wad/UnpackParser.py
@@ -1,3 +1,25 @@
+# Binary Analysis Next Generation (BANG!)
+#
+# This file is part of BANG.
+#
+# BANG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# BANG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License, version 3, along with BANG.  If not, see
+# <http://www.gnu.org/licenses/>
+#
+# Copyright Armijn Hemel
+# Licensed under the terms of the GNU Affero General Public License
+# version 3
+# SPDX-License-Identifier: AGPL-3.0-only
+
 import os
 from UnpackParser import UnpackParser, check_condition
 from UnpackParserException import UnpackParserException
@@ -38,4 +60,3 @@ class DoomWadUnpackParser(WrappedUnpackParser):
     def set_metadata_and_labels(self):
         self.unpack_results.set_labels(['doom', 'wad', 'resource'])
         self.unpack_results.set_metadata({})
-
