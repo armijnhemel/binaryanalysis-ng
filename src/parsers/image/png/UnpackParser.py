8c4f6f8367da7bdfdc139e1b417f5367ed4f5d96
Armijn Hemel <armijn@tjaldur.nl>
2021-03-09 18:08:37 +0100

    PNG: add comments, add license text

--- src/parsers/image/png/UnpackParser.py
+++ src/parsers/image/png/UnpackParser.py
@@ -1,13 +1,46 @@
+# Binary Analysis Next Generation (BANG!)
+#
+# This file is part of BANG.
+#
+# BANG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# BANG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License, version 3, along with BANG.  If not, see
+# <http://www.gnu.org/licenses/>
+#
+# Copyright Armijn Hemel
+# Licensed under the terms of the GNU Affero General Public License
+# version 3
+# SPDX-License-Identifier: AGPL-3.0-only
+
+'''
+Parse and unpack PNG files. The specification of the PNG can be found at:
+
+https://www.w3.org/TR/PNG/
+
+Section 5 describes the structure of a PNG file
+'''
+
 import os
 import binascii
 import datetime
+
 import defusedxml.minidom
 import PIL.Image
 from PIL.ExifTags import TAGS as EXIF_TAGS
+
 from UnpackParser import UnpackParser, check_condition
 from UnpackParserException import UnpackParserException
 from kaitaistruct import ValidationNotEqualError
 from . import png
+
 
 class PngUnpackParser(UnpackParser):
     #extensions = ['.png']
