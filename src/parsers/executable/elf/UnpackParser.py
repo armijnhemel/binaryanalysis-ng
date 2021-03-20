d71fdea075747071fcc6c05edae76fede05273b6
Armijn Hemel <armijn@tjaldur.nl>
2021-03-20 21:01:36 +0100

    ELF: start working on parser based on kaitai struct

--- src/parsers/executable/elf/UnpackParser.py
+++ src/parsers/executable/elf/UnpackParser.py
@@ -1,8 +1,36 @@
+# Binary Analysis Next Generation (BANG!)
+#
+# This file is part of BANG.
+#
+# BANG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# BANG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License, version 3, along with BANG.  If not, see
+# <http://www.gnu.org/licenses/>
+#
+# Copyright Armijn Hemel
+# Licensed under the terms of the GNU Affero General Public License
+# version 3
+# SPDX-License-Identifier: AGPL-3.0-only
+
 
 import os
 from UnpackParser import WrappedUnpackParser
 from bangunpack import unpack_elf
+from UnpackParser import UnpackParser, check_condition
+from UnpackParserException import UnpackParserException
+from kaitaistruct import ValidationNotEqualError
+from . import elf
 
+
+#class ElfUnpackParser(UnpackParser):
 class ElfUnpackParser(WrappedUnpackParser):
     extensions = []
     signatures = [
@@ -13,3 +41,17 @@ class ElfUnpackParser(WrappedUnpackParser):
     def unpack_function(self, fileresult, scan_environment, offset, unpack_dir):
         return unpack_elf(fileresult, scan_environment, offset, unpack_dir)
 
+    def parse(self):
+        self.chunknames = set()
+        try:
+            self.data = elf.Elf.from_io(self.infile)
+        except (Exception, ValidationNotEqualError) as e:
+            raise UnpackParserException(e.args)
+
+    def set_metadata_and_labels(self):
+        """sets metadata and labels for the unpackresults"""
+        labels = [ 'elf' ]
+        metadata = {'security': []}
+
+        self.unpack_results.set_metadata(metadata)
+        self.unpack_results.set_labels(labels)
