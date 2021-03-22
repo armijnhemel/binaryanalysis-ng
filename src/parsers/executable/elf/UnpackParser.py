e37a419b4fbd9b78021211587a24dfb93b0fbaa0
Armijn Hemel <armijn@tjaldur.nl>
2021-03-22 19:41:37 +0100

    ELF: rework processing of sections, calcualte correct size for ELF

--- src/parsers/executable/elf/UnpackParser.py
+++ src/parsers/executable/elf/UnpackParser.py
@@ -46,8 +46,15 @@ class ElfUnpackParser(WrappedUnpackParser):
         self.chunknames = set()
         try:
             self.data = elf.Elf.from_io(self.infile)
+            for header in self.data.header.program_headers:
+                pass
         except (Exception, ValidationNotEqualError) as e:
             raise UnpackParserException(e.args)
+
+    def calculate_unpacked_size(self):
+        self.unpacked_size = self.data.header.section_header_offset
+        for header in self.data.header.section_headers:
+            self.unpacked_size += self.data.header.section_header_entry_size
 
     def set_metadata_and_labels(self):
         """sets metadata and labels for the unpackresults"""
@@ -87,6 +94,7 @@ class ElfUnpackParser(WrappedUnpackParser):
         metadata['machine'] = self.data.header.machine.value
 
         metadata['security'] = []
+        metadata['section_names'] = self.data.header.strings.entries
 
         # keep track of whether or not GNU_RELRO has been set
         seen_relro = False
@@ -115,19 +123,46 @@ class ElfUnpackParser(WrappedUnpackParser):
                 is_dynamic_elf = True
                 for entry in header.body.entries:
                     pass
+            elif header.type == elf.Elf.ShType.strtab:
+                for entry in header.body.entries:
+                    pass
             elif header.type == elf.Elf.ShType.dynsym:
-                pass
+                for entry in header.body.entries:
+                    pass
             elif header.type == elf.Elf.ShType.note:
                 if header.name == '.note.go.buildid':
                     labels.append('go')
-                for h in header.body.entries:
-                    if h.note_name == b'GNU\x00' and h.note_type == 3:
-                        buildid = binascii.hexlify(h.note_description).decode()
+                # Although not common notes sections can be merged
+                # with eachother.
+                for entry in header.body.entries:
+                    if entry.note_name == b'GNU\x00' and entry.note_type == 1:
+                        # https://raw.githubusercontent.com/wiki/hjl-tools/linux-abi/linux-abi-draft.pdf
+                        # normally in .note.ABI.tag
+                        major_version = int.from_bytes(entry.note_description[4:8],
+                                                       byteorder=metadata['endian'])
+                        patchlevel = int.from_bytes(entry.note_description[8:12],
+                                                    byteorder=metadata['endian'])
+                        sublevel = int.from_bytes(entry.note_description[12:],
+                                                  byteorder=metadata['endian'])
+                        metadata['linux_version'] = (major_version, patchlevel, sublevel)
+                    elif entry.note_name == b'GNU\x00' and entry.note_type == 3:
+                        # normally in .note.gnu.build-id
+                        buildid = binascii.hexlify(entry.note_description).decode()
                         metadata['build-id'] = buildid
                         if len(buildid) == 40:
                             metadata['build-id hash'] = 'sha1'
                         elif len(buildid) == 32:
                             metadata['build-id hash'] = 'md5'
+                    elif entry.note_name == b'GNU\x00' and entry.note_type == 5:
+                        # normally in .note.gnu.property
+                        pass
+                    elif entry.note_name == b'Go\x00\x00' and entry.note_type == 4:
+                        # normally in .note.go.buildid
+                        # there are four hashes concatenated
+                        # https://golang.org/pkg/cmd/internal/buildid/#FindAndHash
+                        pass
+                    else:
+                        pass
 
         if is_dynamic_elf:
             labels.append('dynamic')
