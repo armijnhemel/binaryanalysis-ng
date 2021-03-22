00afdb299b44460648664980b0a597a7adf91bda
Armijn Hemel <armijn@tjaldur.nl>
2021-03-22 16:06:58 +0100

    ELF: extract and store different pieces of metadata

--- src/parsers/executable/elf/UnpackParser.py
+++ src/parsers/executable/elf/UnpackParser.py
@@ -22,6 +22,7 @@
 
 
 import os
+import binascii
 from UnpackParser import WrappedUnpackParser
 from bangunpack import unpack_elf
 from UnpackParser import UnpackParser, check_condition
@@ -51,7 +52,41 @@ class ElfUnpackParser(WrappedUnpackParser):
     def set_metadata_and_labels(self):
         """sets metadata and labels for the unpackresults"""
         labels = [ 'elf' ]
-        metadata = {'security': []}
+        metadata = {}
+
+        if self.data.bits == elf.Elf.Bits.b32:
+            metadata['bits'] = 32
+        elif self.data.bits == elf.Elf.Bits.b64:
+            metadata['bits'] = 64
+
+        # store the endianness
+        if self.data.endian == elf.Elf.Endian.le:
+            metadata['endian'] = 'little'
+        elif self.data.endian == elf.Elf.Endian.be:
+            metadata['endian'] = 'big'
+
+        # store the ELF version
+        metadata['version'] = self.data.ei_version
+
+        # store the type of ELF file
+        if self.data.header.e_type == elf.Elf.ObjType.no_file_type:
+            metadata['type'] = None
+        elif self.data.header.e_type == elf.Elf.ObjType.relocatable:
+            metadata['type'] = 'relocatable'
+        elif self.data.header.e_type == elf.Elf.ObjType.executable:
+            metadata['type'] = 'executable'
+        elif self.data.header.e_type == elf.Elf.ObjType.shared:
+            metadata['type'] = 'shared'
+        elif self.data.header.e_type == elf.Elf.ObjType.core:
+            metadata['type'] = 'core'
+        else:
+            metadata['type'] = 'processor specific'
+
+        # store the machine type, both numerical and pretty printed
+        metadata['machine_name'] = self.data.header.machine.name
+        metadata['machine'] = self.data.header.machine.value
+
+        metadata['security'] = []
 
         # keep track of whether or not GNU_RELRO has been set
         seen_relro = False
@@ -67,6 +102,7 @@ class ElfUnpackParser(WrappedUnpackParser):
             elif header.type == elf.Elf.PhType.pax_flags:
                 metadata['security'].append('pax')
 
+        # process the various section headers
         is_dynamic_elf = False
         for header in self.data.header.section_headers:
             if header.name in ['.modinfo', '__ksymtab_strings']:
@@ -74,13 +110,24 @@ class ElfUnpackParser(WrappedUnpackParser):
             elif header.name in ['oat_patches', '.text.oat_patches']:
                 labels.append('oat')
                 labels.append('android')
+
             if header.type == elf.Elf.ShType.dynamic:
                 is_dynamic_elf = True
-            elif header.type == elf.Elf.ShType.note:
-                for h in header.body.entries:
+                for entry in header.body.entries:
                     pass
+            elif header.type == elf.Elf.ShType.dynsym:
+                pass
+            elif header.type == elf.Elf.ShType.note:
                 if header.name == '.note.go.buildid':
                     labels.append('go')
+                for h in header.body.entries:
+                    if h.note_name == b'GNU\x00' and h.note_type == 3:
+                        buildid = binascii.hexlify(h.note_description).decode()
+                        metadata['build-id'] = buildid
+                        if len(buildid) == 40:
+                            metadata['build-id hash'] = 'sha1'
+                        elif len(buildid) == 32:
+                            metadata['build-id hash'] = 'md5'
 
         if is_dynamic_elf:
             labels.append('dynamic')
