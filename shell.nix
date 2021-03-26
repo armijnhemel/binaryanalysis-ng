8867a112903c24a4217295dd4f4098c2c5aafac6
Armijn Hemel <armijn@tjaldur.nl>
2021-03-26 20:33:28 +0100

    clean up shell.nix

--- shell.nix
+++ shell.nix
@@ -3,7 +3,6 @@ let
   # See https://github.com/nmattia/niv/
   sources = import ./nix/sources.nix;
 
-  #pkgs = import sources.nixpkgs { config = {}; overlays = []; };
   pkgs = import sources.nixpkgs { config.allowUnfree = true; overlays = []; };
 
   my-python = pkgs.python3.withPackages (p: with p; [
