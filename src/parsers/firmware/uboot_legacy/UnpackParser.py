7bec1bc27c44d1a5353f1339bc07f9cf40e3f576
Armijn Hemel <armijn@tjaldur.nl>
2021-04-08 12:10:57 +0200

    u-boot: add magic for .bix files

--- src/parsers/firmware/uboot_legacy/UnpackParser.py
+++ src/parsers/firmware/uboot_legacy/UnpackParser.py
@@ -6,7 +6,8 @@ from bangunpack import unpack_uboot_legacy
 class UbootLegacyUnpackParser(WrappedUnpackParser):
     extensions = []
     signatures = [
-        (0, b'\x27\x05\x19\x56')
+        (0, b'\x27\x05\x19\x56'),
+        (0, b'\x83\x80\x00\x00')
     ]
     pretty_name = 'uboot_legacy'
 
