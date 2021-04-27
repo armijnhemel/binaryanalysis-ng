ba2f09a2de06de50db898e07edff476bd43a33f7
Armijn Hemel <armijn@tjaldur.nl>
2021-04-27 22:19:11 +0200

    nix: move dependencies only used for maintainence script to separate .nix file

--- maintenance.nix
+++ maintenance.nix
@@ -6,46 +6,17 @@ let
   pkgs = import sources.nixpkgs { config.allowUnfree = true; overlays = []; };
 
   my-python = pkgs.python3.withPackages (p: with p; [
-    deepdiff
     defusedxml
-    dockerfile-parse
-    elasticsearch
-    icalendar
-    kaitaistruct
     lz4
-    parameterized
-    pdfminer
-    pefile
-    pillow
     psycopg2
     pytest
-    python-snappy
     pyyaml
-    tinycss2
-    tlsh
+    requests
   ]);
     
 in
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    binutils
-    cabextract
-    e2tools
-    innoextract
-    libxml2
-    lz4
-    lzop
-    mailcap
-    ncompress
-    openjdk8
-    openssl
     my-python
-    qemu
-    rzip
-    squashfsTools
-    unrar
-    unshield
-    utillinux
-    zstd
   ];
 }
