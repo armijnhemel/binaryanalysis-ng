6794fae45b4c33b6cb9cfa4cd38e1dd5effc4c19
Armijn Hemel <armijn@tjaldur.nl>
2021-05-02 18:53:07 +0200

    cpio: update test, add new test

--- src/parsers/archivers/cpio/test_parser.py
+++ src/parsers/archivers/cpio/test_parser.py
@@ -123,7 +123,31 @@ def test_cpio_with_absolute_path(scan_environment):
     assert extracted_labels == set()
 
 
-def test_load_cpio_with_multiple_files(scan_environment):
+def test_load_cpio_with_multiple_files_crc(scan_environment):
+    rel_testfile = pathlib.Path('unpackers') / 'cpio' / 'test-crc-multiple-files.cpio'
+    copy_testfile_to_environment(testdir_base / 'testdata', rel_testfile, scan_environment)
+    fr = fileresult(testdir_base / 'testdata', rel_testfile, set())
+    filesize = fr.filesize
+    data_unpack_dir = rel_testfile.parent / ('unpack-'+rel_testfile.name + "-1")
+    p = CpioNewAsciiUnpackParser(fr, scan_environment, data_unpack_dir, 0)
+    p.open()
+    r = p.parse_and_unpack()
+    p.close()
+    assert r.get_length() <= filesize
+    assert len(r.get_unpacked_files()) == 2
+    assert r.get_unpacked_files()[0].filename == data_unpack_dir / 'example.hex'
+    assert r.get_unpacked_files()[1].filename == data_unpack_dir / 'example.txt'
+    assertUnpackedPathExists(scan_environment, data_unpack_dir / 'example.hex')
+    assertUnpackedPathExists(scan_environment, data_unpack_dir / 'example.txt')
+    extracted_fn_abs_1 = pathlib.Path(scan_environment.unpackdirectory) / data_unpack_dir / 'example.hex'
+    extracted_fn_abs_2 = pathlib.Path(scan_environment.unpackdirectory) / data_unpack_dir / 'example.txt'
+    s1 = open(extracted_fn_abs_1,"rb").read()
+    s2 = open(extracted_fn_abs_2,"rb").read()
+    assert s1 == s2
+    # with open(extracted_fn_abs,"rb") as f:
+    #    assert f.read(2) == b'\x01\xda'
+
+def test_load_cpio_with_multiple_files_new_ascii(scan_environment):
     rel_testfile = pathlib.Path('unpackers') / 'cpio' / 'test-new-multiple-files.cpio'
     copy_testfile_to_environment(testdir_base / 'testdata', rel_testfile, scan_environment)
     fr = fileresult(testdir_base / 'testdata', rel_testfile, set())
@@ -135,12 +159,12 @@ def test_load_cpio_with_multiple_files(scan_environment):
     p.close()
     assert r.get_length() <= filesize
     assert len(r.get_unpacked_files()) == 2
-    assert r.get_unpacked_files()[0].filename == data_unpack_dir / 'cpio' / 'example.hex'
-    assert r.get_unpacked_files()[1].filename == data_unpack_dir / 'cpio' / 'example.txt'
-    assertUnpackedPathExists(scan_environment, data_unpack_dir / 'cpio' / 'example.hex')
-    assertUnpackedPathExists(scan_environment, data_unpack_dir / 'cpio' / 'example.txt')
-    extracted_fn_abs_1 = pathlib.Path(scan_environment.unpackdirectory) / data_unpack_dir / 'cpio' / 'example.hex'
-    extracted_fn_abs_2 = pathlib.Path(scan_environment.unpackdirectory) / data_unpack_dir / 'cpio' / 'example.txt'
+    assert r.get_unpacked_files()[0].filename == data_unpack_dir / 'example.hex'
+    assert r.get_unpacked_files()[1].filename == data_unpack_dir / 'example.txt'
+    assertUnpackedPathExists(scan_environment, data_unpack_dir / 'example.hex')
+    assertUnpackedPathExists(scan_environment, data_unpack_dir / 'example.txt')
+    extracted_fn_abs_1 = pathlib.Path(scan_environment.unpackdirectory) / data_unpack_dir / 'example.hex'
+    extracted_fn_abs_2 = pathlib.Path(scan_environment.unpackdirectory) / data_unpack_dir / 'example.txt'
     s1 = open(extracted_fn_abs_1,"rb").read()
     s2 = open(extracted_fn_abs_2,"rb").read()
     assert s1 == s2
