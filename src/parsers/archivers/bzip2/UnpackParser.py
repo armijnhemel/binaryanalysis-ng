2e556636615c60009380a0f97423532d312d3ae2
Armijn Hemel <armijn@tjaldur.nl>
2021-07-27 19:48:41 +0200

    bzip2: expand signatures to avoid false positives

--- src/parsers/archivers/bzip2/UnpackParser.py
+++ src/parsers/archivers/bzip2/UnpackParser.py
@@ -6,7 +6,16 @@ from bangunpack import unpack_bzip2
 class Bzip2UnpackParser(WrappedUnpackParser):
     extensions = []
     signatures = [
-        (0, b'BZh')
+        (0, b'BZh01AY&SY'),
+        (1, b'BZh11AY&SY'),
+        (2, b'BZh21AY&SY'),
+        (3, b'BZh31AY&SY'),
+        (4, b'BZh41AY&SY'),
+        (5, b'BZh51AY&SY'),
+        (6, b'BZh61AY&SY'),
+        (7, b'BZh71AY&SY'),
+        (8, b'BZh81AY&SY'),
+        (9, b'BZh91AY&SY')
     ]
     pretty_name = 'bzip2'
 
