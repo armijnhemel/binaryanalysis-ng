cf3504bcb5071ac7f64000c9fd0e71033249dc57
Armijn Hemel <armijn@tjaldur.nl>
2021-08-18 15:20:24 +0200

    ico: disable some checks that are not correct, add some extra checks for the bitmapheader

--- src/parsers/image/ico/UnpackParser.py
+++ src/parsers/image/ico/UnpackParser.py
@@ -44,14 +44,21 @@ class IcoUnpackParser(UnpackParser):
         for img in self.data.images:
             try:
                 bmp_header = img.bmp
+                if bmp_header is not None:
+                    check_condition(img.width == bmp_header.width,
+                                    "width in icon dir and bmp header not matching")
+                    check_condition(img.height * 2 == bmp_header.height,
+                                    "height in icon dir and bmp header not matching")
             except (Exception, ValidationNotEqualError, ValidationNotAnyOfError) as e:
                 raise UnpackParserException(e.args)
             #check_condition(img.num_colors > 0,
                     #"Invalid ico file: zero or negative num_colors")
-            check_condition(img.num_planes > 0,
-                    "Invalid ico file: zero or negative num_planes")
-            check_condition(img.bpp > 0,
-                    "Invalid ico file: zero or negative bpp")
+            # specifications are often not followed for num_planes and bpp:
+            # https://devblogs.microsoft.com/oldnewthing/20101018-00/?p=12513
+            #check_condition(img.num_planes > 0,
+                    #"Invalid ico file: zero or negative num_planes")
+            #check_condition(img.bpp > 0,
+                    #"Invalid ico file: zero or negative bpp")
             check_condition(img.ofs_img + img.len_img <= self.fileresult.filesize,
                     "Invalid ico file: image outside of file")
             check_condition(img.ofs_img >= 6 + self.data.num_images * 16,
