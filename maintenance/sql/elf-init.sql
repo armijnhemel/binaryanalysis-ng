ef84a480ee5241b9333f5e302a91189f8e67ce5e
Armijn Hemel <armijn@tjaldur.nl>
2021-08-24 22:07:11 +0200

    elf sql: don't use hash, but use a unique index

--- maintenance/sql/elf-init.sql
+++ maintenance/sql/elf-init.sql
@@ -1,2 +1,2 @@
 CREATE TABLE IF NOT EXISTS elf_hashes(sha256 text, tlsh text, telfhash text);
-CREATE INDEX elf_hashes_sha256 ON elf_hashes USING HASH (sha256);
+CREATE UNIQUE INDEX elf_hashes_sha256 ON elf_hashes (sha256);
