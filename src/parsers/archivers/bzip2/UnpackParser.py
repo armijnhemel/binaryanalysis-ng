383f66b845938c68230b225c9ecb3552c8c91761
Armijn Hemel <armijn@tjaldur.nl>
2021-08-30 21:02:13 +0200

    bzip2: fix offsets (d'oh!)

--- src/parsers/archivers/bzip2/UnpackParser.py
+++ src/parsers/archivers/bzip2/UnpackParser.py
@@ -7,15 +7,15 @@ class Bzip2UnpackParser(WrappedUnpackParser):
     extensions = []
     signatures = [
         (0, b'BZh01AY&SY'),
-        (1, b'BZh11AY&SY'),
-        (2, b'BZh21AY&SY'),
-        (3, b'BZh31AY&SY'),
-        (4, b'BZh41AY&SY'),
-        (5, b'BZh51AY&SY'),
-        (6, b'BZh61AY&SY'),
-        (7, b'BZh71AY&SY'),
-        (8, b'BZh81AY&SY'),
-        (9, b'BZh91AY&SY')
+        (0, b'BZh11AY&SY'),
+        (0, b'BZh21AY&SY'),
+        (0, b'BZh31AY&SY'),
+        (0, b'BZh41AY&SY'),
+        (0, b'BZh51AY&SY'),
+        (0, b'BZh61AY&SY'),
+        (0, b'BZh71AY&SY'),
+        (0, b'BZh81AY&SY'),
+        (0, b'BZh91AY&SY')
     ]
     pretty_name = 'bzip2'
 
