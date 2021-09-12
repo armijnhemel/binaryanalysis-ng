b18e3271c74b0a0673b3ef15b0a9148689a64a02
Armijn Hemel <armijn@tjaldur.nl>
2021-09-12 23:02:40 +0200

    png: catch more exceptions

--- src/parsers/image/png/UnpackParser.py
+++ src/parsers/image/png/UnpackParser.py
@@ -41,7 +41,7 @@ import PIL.Image
 
 from UnpackParser import UnpackParser, check_condition
 from UnpackParserException import UnpackParserException
-from kaitaistruct import ValidationNotEqualError, ValidationExprError
+from kaitaistruct import ValidationNotEqualError, ValidationExprError, ValidationLessThanError
 from . import png
 
 # a list of known chunks
@@ -67,7 +67,7 @@ class PngUnpackParser(UnpackParser):
         self.chunknames = set()
         try:
             self.data = png.Png.from_io(self.infile)
-        except (Exception, ValidationNotEqualError, ValidationExprError) as e:
+        except (Exception, ValidationNotEqualError, ValidationExprError, ValidationLessThanError) as e:
             raise UnpackParserException(e.args)
         check_condition(self.data.ihdr.bit_depth in [1, 2, 4, 8, 16],
                 "invalid bit depth")
