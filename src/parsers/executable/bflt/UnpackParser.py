2652b460f704d2d9098aa0184e159ce98290d3c8
Armijn Hemel <armijn@tjaldur.nl>
2021-10-21 19:29:11 +0200

    bflt: start on replacing parser

--- src/parsers/executable/bflt/UnpackParser.py
+++ src/parsers/executable/bflt/UnpackParser.py
@@ -1,9 +1,38 @@
+# Binary Analysis Next Generation (BANG!)
+#
+# This file is part of BANG.
+#
+# BANG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# BANG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License, version 3, along with BANG.  If not, see
+# <http://www.gnu.org/licenses/>
+#
+# Copyright Armijn Hemel
+# Licensed under the terms of the GNU Affero General Public License
+# version 3
+# SPDX-License-Identifier: AGPL-3.0-only
+
 
 import os
 from UnpackParser import WrappedUnpackParser
 from bangunpack import unpack_bflt
 
+from UnpackParser import UnpackParser, check_condition
+from UnpackParserException import UnpackParserException
+from kaitaistruct import ValidationFailedError
+#from . import bflt
+
+
 class BfltUnpackParser(WrappedUnpackParser):
+#class BfltUnpackParser(UnpackParser):
     extensions = []
     signatures = [
         (0, b'bFLT')
@@ -13,3 +42,20 @@ class BfltUnpackParser(WrappedUnpackParser):
     def unpack_function(self, fileresult, scan_environment, offset, unpack_dir):
         return unpack_bflt(fileresult, scan_environment, offset, unpack_dir)
 
+    def parse(self):
+        try:
+            self.data = bflt.Bflt.from_io(self.infile)
+        except (Exception, ValidationFailedError) as e:
+            raise UnpackParserException(e.args)
+
+    # no need to carve from the file
+    def carve(self):
+        pass
+
+    def set_metadata_and_labels(self):
+        """sets metadata and labels for the unpackresults"""
+        labels = ['bflt', 'executable']
+        metadata = {}
+
+        self.unpack_results.set_labels(labels)
+        self.unpack_results.set_metadata(metadata)
