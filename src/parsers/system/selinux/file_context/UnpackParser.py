5bd8d82d4ea4be4132082cdc6dbad200020bcf68
Armijn Hemel <armijn@tjaldur.nl>
2021-11-01 13:54:31 +0100

    selinux file contexts: rename file_contexts

--- src/parsers/system/selinux/file_context/UnpackParser.py
+++ src/parsers/system/selinux/file_context/UnpackParser.py
@@ -27,20 +27,20 @@ from FileResult import FileResult
 from UnpackParser import UnpackParser, check_condition
 from UnpackParserException import UnpackParserException
 from kaitaistruct import ValidationFailedError
-from . import file_contexts
+from . import selinux_file_contexts
 
 
-class FileContext(UnpackParser):
+class SELinuxFileContext(UnpackParser):
     extensions = []
     signatures = [
         (0, b'\x8a\xff\x7c\xf9')
     ]
-    pretty_name = 'file_contexts'
+    pretty_name = 'selinux_file_contexts'
 
     def parse(self):
         file_size = self.fileresult.filesize
         try:
-            self.data = file_contexts.FileContexts.from_io(self.infile)
+            self.data = selinux_file_contexts.SelinuxFileContexts.from_io(self.infile)
         except (Exception, ValidationFailedError) as e:
             raise UnpackParserException(e.args)
 
