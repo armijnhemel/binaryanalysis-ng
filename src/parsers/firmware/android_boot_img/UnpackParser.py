6f1742e77e01b17c2735edfd32d0743a62d6a178
Armijn Hemel <armijn@tjaldur.nl>
2021-11-10 18:59:35 +0100

    android_boot_img: support variant from Qualcomm(?) as well

--- src/parsers/firmware/android_boot_img/UnpackParser.py
+++ src/parsers/firmware/android_boot_img/UnpackParser.py
@@ -24,11 +24,6 @@
 Unpacker for Android images.
 '''
 
-# TODO: there is a variation of this format that is like
-# version 0 but which uses dtb. This is not yet supported
-# because of lack of test files.
-#
-# https://github.com/M1cha/android_bootable_bootloader_lk/blob/condor/app/aboot/bootimg.h
 
 import os
 import pathlib
@@ -37,6 +32,7 @@ from UnpackParser import UnpackParser, check_condition
 from UnpackParserException import UnpackParserException
 from kaitaistruct import ValidationFailedError
 from . import android_img
+from . import android_img_lk
 
 
 class AndroidBootImgUnpacker(UnpackParser):
@@ -49,35 +45,45 @@ class AndroidBootImgUnpacker(UnpackParser):
 
     def parse(self):
         file_size = self.fileresult.filesize
+        self.is_variant = False
         try:
             self.data = android_img.AndroidImg.from_io(self.infile)
         except (Exception, ValidationFailedError) as e:
-            raise UnpackParserException(e.args)
+            try:
+                self.infile.seek(self.offset)
+                self.data = android_img_lk.AndroidImgLk.from_io(self.infile)
+                self.is_variant = True
+            except (Exception, ValidationFailedError) as e:
+                raise UnpackParserException(e.args)
+        print(self.data.header_version, self.is_variant)
 
         self.unpacked_size = self.infile.tell()
 
         # compute the size and check against the file size
         # take padding into account
-        if self.data.header_version < 3:
-            page_size = self.data.header.page_size
-            unpacked_size = ((page_size + self.data.header.kernel.size + page_size - 1)//page_size) * page_size
-            if self.data.header.ramdisk.size > 0:
-                unpacked_size = ((unpacked_size + self.data.header.ramdisk.size + page_size - 1)//page_size) * page_size
-            if self.data.header.second.size > 0:
-                unpacked_size = ((unpacked_size + self.data.header.second.size + page_size - 1)//page_size) * page_size
-            if self.data.header_version > 0:
-                if self.data.header.recovery_dtbo.size > 0:
-                    unpacked_size = ((self.data.header.recovery_dtbo.offset + self.data.header.recovery_dtbo.size + page_size - 1)//page_size) * page_size
-            if self.data.header_version > 1:
-                if self.data.header.dtb.size > 0:
-                    unpacked_size = ((unpacked_size + self.data.header.dtb.size + page_size - 1)//page_size) * page_size
-            self.unpacked_size = max(self.unpacked_size, unpacked_size)
-            check_condition(file_size >= self.unpacked_size, "not enough data")
+        if self.is_variant:
+            self.unpacked_size = max(self.unpacked_size, self.data.header.dtb_pos + self.data.header.dt_size)
         else:
-            unpacked_size = 4096 + len(self.data.header.kernel_img) + \
-                len(self.data.header.padding1) + len(self.data.header.ramdisk_img) + \
-                len(self.data.header.padding2)
-            self.unpacked_size = max(self.unpacked_size, unpacked_size)
+            if self.data.header_version < 3:
+                page_size = self.data.header.page_size
+                unpacked_size = ((page_size + self.data.header.kernel.size + page_size - 1)//page_size) * page_size
+                if self.data.header.ramdisk.size > 0:
+                    unpacked_size = ((unpacked_size + self.data.header.ramdisk.size + page_size - 1)//page_size) * page_size
+                if self.data.header.second.size > 0:
+                    unpacked_size = ((unpacked_size + self.data.header.second.size + page_size - 1)//page_size) * page_size
+                if self.data.header_version > 0:
+                    if self.data.header.recovery_dtbo.size > 0:
+                        unpacked_size = ((self.data.header.recovery_dtbo.offset + self.data.header.recovery_dtbo.size + page_size - 1)//page_size) * page_size
+                if self.data.header_version > 1:
+                    if self.data.header.dtb.size > 0:
+                        unpacked_size = ((unpacked_size + self.data.header.dtb.size + page_size - 1)//page_size) * page_size
+                self.unpacked_size = max(self.unpacked_size, unpacked_size)
+                check_condition(file_size >= self.unpacked_size, "not enough data")
+            elif self.data.header_version in [3, 4]:
+                unpacked_size = 4096 + len(self.data.header.kernel_img) + \
+                    len(self.data.header.padding1) + len(self.data.header.ramdisk_img) + \
+                    len(self.data.header.padding2)
+                self.unpacked_size = max(self.unpacked_size, unpacked_size)
 
     # no need to carve from the file
     def carve(self):
@@ -116,7 +122,8 @@ class AndroidBootImgUnpacker(UnpackParser):
             outfile.close()
             fr = FileResult(self.fileresult, outfile_rel, set([]))
             unpacked_files.append(fr)
-        if self.data.header_version < 3:
+
+        if self.is_variant:
             if self.data.header.second.size > 0:
                 outfile_rel = self.rel_unpack_dir / secondstage_name
                 outfile_full = self.scan_environment.unpack_path(outfile_rel)
@@ -126,44 +133,68 @@ class AndroidBootImgUnpacker(UnpackParser):
                 outfile.close()
                 fr = FileResult(self.fileresult, outfile_rel, set([]))
                 unpacked_files.append(fr)
-            if self.data.header_version > 0:
-                if self.data.header.recovery_dtbo.size > 0:
-                    outfile_rel = self.rel_unpack_dir / recovery_name
+            if self.data.header.dt_size> 0:
+                outfile_rel = self.rel_unpack_dir / dtb_name
+                outfile_full = self.scan_environment.unpack_path(outfile_rel)
+                os.makedirs(outfile_full.parent, exist_ok=True)
+                outfile = open(outfile_full, 'wb')
+                outfile.write(self.data.header.dtb)
+                outfile.close()
+                fr = FileResult(self.fileresult, outfile_rel, set([]))
+                unpacked_files.append(fr)
+        else:
+            if self.data.header_version < 3:
+                if self.data.header.second.size > 0:
+                    outfile_rel = self.rel_unpack_dir / secondstage_name
                     outfile_full = self.scan_environment.unpack_path(outfile_rel)
                     os.makedirs(outfile_full.parent, exist_ok=True)
                     outfile = open(outfile_full, 'wb')
-                    outfile.write(self.data.header.recovery_dtbo_img)
+                    outfile.write(self.data.header.second_img)
                     outfile.close()
                     fr = FileResult(self.fileresult, outfile_rel, set([]))
                     unpacked_files.append(fr)
-            if self.data.header_version > 1:
-                if self.data.header.dtb.size > 0:
-                    outfile_rel = self.rel_unpack_dir / dtb_name
-                    outfile_full = self.scan_environment.unpack_path(outfile_rel)
-                    os.makedirs(outfile_full.parent, exist_ok=True)
-                    outfile = open(outfile_full, 'wb')
-                    outfile.write(self.data.header.dtb_img)
-                    outfile.close()
-                    fr = FileResult(self.fileresult, outfile_rel, set([]))
-                    unpacked_files.append(fr)
+                if self.data.header_version > 0:
+                    if self.data.header.recovery_dtbo.size > 0:
+                        outfile_rel = self.rel_unpack_dir / recovery_name
+                        outfile_full = self.scan_environment.unpack_path(outfile_rel)
+                        os.makedirs(outfile_full.parent, exist_ok=True)
+                        outfile = open(outfile_full, 'wb')
+                        outfile.write(self.data.header.recovery_dtbo_img)
+                        outfile.close()
+                        fr = FileResult(self.fileresult, outfile_rel, set([]))
+                        unpacked_files.append(fr)
+                if self.data.header_version > 1:
+                    if self.data.header.dtb.size > 0:
+                        outfile_rel = self.rel_unpack_dir / dtb_name
+                        outfile_full = self.scan_environment.unpack_path(outfile_rel)
+                        os.makedirs(outfile_full.parent, exist_ok=True)
+                        outfile = open(outfile_full, 'wb')
+                        outfile.write(self.data.header.dtb_img)
+                        outfile.close()
+                        fr = FileResult(self.fileresult, outfile_rel, set([]))
+                        unpacked_files.append(fr)
         return unpacked_files
 
     def set_metadata_and_labels(self):
         """sets metadata and labels for the unpackresults"""
         labels = [ 'android', "android boot image"]
+        if self.variant:
+            labels.add('lk variant')
         metadata = {'version': self.data.header_version}
+
         if self.data.header.name != '':
             metadata = {'name': self.data.header.name}
         if self.data.header.cmdline != '':
             metadata = {'cmdline': self.data.header.cmdline}
-        if self.data.header.extra_cmdline != '':
-            metadata = {'extra_cmdline': self.data.header.extra_cmdline}
-        metadata['version'] = {'major': self.data.header.os_version.major,
-                               'minor': self.data.header.os_version.minor,
-                               'patch': self.data.header.os_version.patch,
-                               'year': self.data.header.os_version.year,
-                               'month': self.data.header.os_version.month,
-                              }
+        if not self.is_variant:
+            if self.data.header.extra_cmdline != '':
+                metadata = {'extra_cmdline': self.data.header.extra_cmdline}
+            metadata['version'] = {'major': self.data.header.os_version.major,
+                                   'minor': self.data.header.os_version.minor,
+                                   'patch': self.data.header.os_version.patch,
+                                   'year': self.data.header.os_version.year,
+                                   'month': self.data.header.os_version.month,
+                                  }
 
         self.unpack_results.set_metadata(metadata)
         self.unpack_results.set_labels(labels)
