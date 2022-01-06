d544dfc1f9df95acd42b83ae84d4e3508328431f
Armijn Hemel <armijn@tjaldur.nl>
2022-01-06 14:00:57 +0100

    iso9660: set unpacked size, start traversing directory records

--- src/parsers/filesystem/iso9660/UnpackParser.py
+++ src/parsers/filesystem/iso9660/UnpackParser.py
@@ -21,8 +21,10 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 
+import collections
 import os
 import pathlib
+import sys
 
 from UnpackParser import WrappedUnpackParser
 from bangfilesystems import unpack_iso9660
@@ -50,18 +52,73 @@ class Iso9660UnpackParser(WrappedUnpackParser):
         try:
             self.data = iso9660.Iso9660.from_io(self.infile)
 
-            # check the contents of the ISO image, also find out where it ends
-            for volume_descriptor in self.data.data_area:
-                if volume_descriptor.type == iso9660.Iso9660.VolumeType.primary_volume_descriptor:
-                    iso_size = volume_descriptor.volume.volume_space_size.value * volume_descriptor.volume.logical_block_size.value
+            has_primary = False
+            has_terminator = False
+
+            # check the contents of the ISO image
+            for descriptor in self.data.data_area:
+                if descriptor.type == iso9660.Iso9660.VolumeType.primary:
+                    # sanity checks: dates. This does not apply
+                    # to all dates used in the specification.
+                    check_condition(descriptor.volume.volume_creation_date_and_time.valid_date,
+                                    "invalid creation date")
+                    check_condition(descriptor.volume.volume_modification_date_and_time.valid_date,
+                                    "invalid modification date")
+                    check_condition(descriptor.volume.volume_effective_date_and_time.valid_date,
+                                    "invalid effective date")
+
+                    has_primary = True
+                    iso_size = descriptor.volume.volume_space_size.value * descriptor.volume.logical_block_size.value
                     check_condition(iso_size <= self.fileresult.filesize,
                                     "declared ISO9660 image bigger than file")
-                elif volume_descriptor.type == iso9660.Iso9660.VolumeType.boot_record_volume_descriptor:
+
+                    extent_size = descriptor.volume.root_directory.body.extent.value * descriptor.volume.logical_block_size.value
+                    check_condition(extent_size <= self.fileresult.filesize,
+                                    "extent cannot be outside of file")
+
+                    files = collections.deque()
+                    if descriptor.volume.root_directory.body.directory_records is not None:
+                        for record in descriptor.volume.root_directory.body.directory_records.records:
+                            if record.len_dr == 0:
+                                continue
+                            if record.body.file_flags_directory:
+                                if record.body.file_id_dir not in ['\x00', '\x01']:
+                                    files.append(record)
+                            else:
+                                files.append(record)
+
+                    while(len(files) != 0):
+                        record = files.popleft()
+                        extent_size = record.body.extent.value * descriptor.volume.logical_block_size.value
+                        check_condition(extent_size <= self.fileresult.filesize,
+                                        "extent cannot be outside of file")
+
+                        if record.body.directory_records is None:
+                            continue
+                        for dir_record in record.body.directory_records.records:
+                            if dir_record.len_dr == 0:
+                                continue
+                            if dir_record.body.file_flags_directory:
+                                if dir_record.body.file_id_dir not in ['\x00', '\x01']:
+                                    files.append(dir_record)
+                            else:
+                                files.append(dir_record)
+                elif descriptor.type == iso9660.Iso9660.VolumeType.boot_record:
                     pass
+                elif descriptor.type == iso9660.Iso9660.VolumeType.set_terminator:
+                    # there should be at least one volume descriptor set terminator
+                    has_terminator = True
         except (Exception, ValidationFailedError, UndecidedEndiannessError) as e:
             raise UnpackParserException(e.args)
 
+        check_condition(has_primary, "no primary volume descriptor found")
+        check_condition(has_terminator, "no volume descriptor set terminator found")
+        self.unpacked_size = iso_size
 
+
+    # make sure that self.unpacked_size is not overwritten
+    def calculate_unpacked_size(self):
+        pass
 
     def set_metadata_and_labels(self):
         """sets metadata and labels for the unpackresults"""
@@ -69,10 +126,10 @@ class Iso9660UnpackParser(WrappedUnpackParser):
         metadata = {}
 
         for volume_descriptor in self.data.data_area:
-            if volume_descriptor.type == iso9660.Iso9660.VolumeType.primary_volume_descriptor:
+            if volume_descriptor.type == iso9660.Iso9660.VolumeType.primary:
                 pass
-            elif volume_descriptor.type == iso9660.Iso9660.VolumeType.boot_record_volume_descriptor:
-                pass
+            elif volume_descriptor.type == iso9660.Iso9660.VolumeType.boot_record:
+                metadata['bootable'] = True
 
         self.unpack_results.set_metadata(metadata)
         self.unpack_results.set_labels(labels)
