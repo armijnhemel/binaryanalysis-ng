11e858a05a28b39ac56b6b62aca32b46fcb543dd
Armijn Hemel <armijn@tjaldur.nl>
2022-02-08 12:16:57 +0100

    tar: add license header, remove debugging code

--- src/parsers/archivers/tar/UnpackParser.py
+++ src/parsers/archivers/tar/UnpackParser.py
@@ -1,3 +1,25 @@
+# Binary Analysis Next Generation (BANG!)
+#
+# This file is part of BANG.
+#
+# BANG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# BANG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License, version 3, along with BANG.  If not, see
+# <http://www.gnu.org/licenses/>
+#
+# Copyright Armijn Hemel
+# Licensed under the terms of the GNU Affero General Public License
+# version 3
+# SPDX-License-Identifier: AGPL-3.0-only
+
 
 import os
 import pathlib
@@ -30,7 +52,6 @@ class TarUnpackParser(UnpackParser):
 
     def tar_unpack_regular(self, outfile_rel, tarinfo):
         # TODO: absolute paths
-        #print(outfile_rel)
         if outfile_rel.is_absolute():
             raise UnpackParserException("trying to extract to absolute path")
         else:
@@ -69,5 +90,3 @@ class TarUnpackParser(UnpackParser):
             self.tarinfos = self.unpacktar.getmembers()
         except tarfile.TarError as e:
             raise UnpackParserException(e.args)
-
-
