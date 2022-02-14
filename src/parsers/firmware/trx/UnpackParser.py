567ba411e3512fe2ac7cdce71c60b6b6466da583
Armijn Hemel <armijn@tjaldur.nl>
2022-02-14 15:21:03 +0100

    trx: replace old parser with kaitai struct based parser

--- src/parsers/firmware/trx/UnpackParser.py
+++ src/parsers/firmware/trx/UnpackParser.py
@@ -1,9 +1,39 @@
+# Binary Analysis Next Generation (BANG!)
+#
+# This file is part of BANG.
+#
+# BANG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# BANG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License, version 3, along with BANG.  If not, see
+# <http://www.gnu.org/licenses/>
+#
+# Copyright Armijn Hemel
+# Licensed under the terms of the GNU Affero General Public License
+# version 3
+# SPDX-License-Identifier: AGPL-3.0-only
+
 
 import os
-from UnpackParser import WrappedUnpackParser
-from bangunpack import unpack_trx
+import pathlib
+import zlib
 
-class TrxUnpackParser(WrappedUnpackParser):
+from FileResult import FileResult
+
+from UnpackParser import UnpackParser, check_condition
+from UnpackParserException import UnpackParserException
+from kaitaistruct import ValidationFailedError
+from . import trx
+
+
+class TrxUnpackParser(UnpackParser):
     extensions = []
     signatures = [
         (0, b'HDR0')
@@ -13,3 +43,72 @@ class TrxUnpackParser(WrappedUnpackParser):
     def unpack_function(self, fileresult, scan_environment, offset, unpack_dir):
         return unpack_trx(fileresult, scan_environment, offset, unpack_dir)
 
+    def parse(self):
+        try:
+            self.data = trx.Trx.from_io(self.infile)
+        except (Exception, ValidationFailedError) as e:
+            raise UnpackParserException(e.args)
+
+        computed_crc = ~zlib.crc32(self.data.raw_data) & 0xffffffff
+        check_condition(self.data.preheader.crc32 == computed_crc,
+                        "invalid CRC32")
+
+    # no need to carve from the file
+    def carve(self):
+        pass
+
+    def unpack(self):
+        unpacked_files = []
+        if self.data.header_and_data.header.ofs_partition0 != 0:
+            out_labels = []
+            file_path = pathlib.Path("partition0")
+            outfile_rel = self.rel_unpack_dir / file_path
+            outfile_full = self.scan_environment.unpack_path(outfile_rel)
+            os.makedirs(outfile_full.parent, exist_ok=True)
+            outfile = open(outfile_full, 'wb')
+            outfile.write(self.data.header_and_data.header.partition0)
+            outfile.close()
+            fr = FileResult(self.fileresult, self.rel_unpack_dir / file_path, set(out_labels))
+            unpacked_files.append(fr)
+        if self.data.header_and_data.header.ofs_partition1 != 0:
+            out_labels = []
+            file_path = pathlib.Path("partition1")
+            outfile_rel = self.rel_unpack_dir / file_path
+            outfile_full = self.scan_environment.unpack_path(outfile_rel)
+            os.makedirs(outfile_full.parent, exist_ok=True)
+            outfile = open(outfile_full, 'wb')
+            outfile.write(self.data.header_and_data.header.partition1)
+            outfile.close()
+            fr = FileResult(self.fileresult, self.rel_unpack_dir / file_path, set(out_labels))
+            unpacked_files.append(fr)
+        if self.data.header_and_data.header.ofs_partition2 != 0:
+            out_labels = []
+            file_path = pathlib.Path("partition2")
+            outfile_rel = self.rel_unpack_dir / file_path
+            outfile_full = self.scan_environment.unpack_path(outfile_rel)
+            os.makedirs(outfile_full.parent, exist_ok=True)
+            outfile = open(outfile_full, 'wb')
+            outfile.write(self.data.header_and_data.header.partition2)
+            outfile.close()
+            fr = FileResult(self.fileresult, self.rel_unpack_dir / file_path, set(out_labels))
+            unpacked_files.append(fr)
+        if self.data.header_and_data.header.ofs_partition3 != 0:
+            out_labels = []
+            file_path = pathlib.Path("partition3")
+            outfile_rel = self.rel_unpack_dir / file_path
+            outfile_full = self.scan_environment.unpack_path(outfile_rel)
+            os.makedirs(outfile_full.parent, exist_ok=True)
+            outfile = open(outfile_full, 'wb')
+            outfile.write(self.data.header_and_data.header.partition3)
+            outfile.close()
+            fr = FileResult(self.fileresult, self.rel_unpack_dir / file_path, set(out_labels))
+            unpacked_files.append(fr)
+        return unpacked_files
+
+    def set_metadata_and_labels(self):
+        """sets metadata and labels for the unpackresults"""
+        labels = ['trx', 'firmware', 'broadcom']
+        metadata = {}
+
+        self.unpack_results.set_labels(labels)
+        self.unpack_results.set_metadata(metadata)
