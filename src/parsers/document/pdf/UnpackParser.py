df184b4d956926aaf4ab926202f286477948247d
Armijn Hemel <armijn@tjaldur.nl>
2022-02-18 15:36:18 +0100

    pdf: start on rewriting the old PDF parser

--- src/parsers/document/pdf/UnpackParser.py
+++ src/parsers/document/pdf/UnpackParser.py
@@ -1,9 +1,36 @@
+# Binary Analysis Next Generation (BANG!)
+#
+# This file is part of BANG.
+#
+# BANG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# BANG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License, version 3, along with BANG.  If not, see
+# <http://www.gnu.org/licenses/>
+#
+# Copyright Armijn Hemel
+# Licensed under the terms of the GNU Affero General Public License
+# version 3
+# SPDX-License-Identifier: AGPL-3.0-only
+
 
 import os
+
+from UnpackParser import UnpackParser, check_condition
+from UnpackParserException import UnpackParserException
+
 from UnpackParser import WrappedUnpackParser
 from bangmedia import unpack_pdf
 
 class PdfUnpackParser(WrappedUnpackParser):
+#class PdfUnpackParser(UnpackParser):
     extensions = []
     signatures = [
         (0, b'%PDF-')
@@ -13,3 +40,20 @@ class PdfUnpackParser(WrappedUnpackParser):
     def unpack_function(self, fileresult, scan_environment, offset, unpack_dir):
         return unpack_pdf(fileresult, scan_environment, offset, unpack_dir)
 
+    def parse(self):
+        pass
+
+    def extract_metadata_and_labels(self):
+        '''Extract metadata from the PDF file and set labels'''
+        labels = ['pdf']
+        metadata = {}
+
+        return(labels, metadata)
+
+    def set_metadata_and_labels(self):
+        """sets metadata and labels for the unpackresults"""
+        metadata = {}
+
+        (labels, metadata) = self.extract_metadata_and_labels()
+        self.unpack_results.set_metadata(metadata)
+        self.unpack_results.set_labels(labels)
