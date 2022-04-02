5d92681b46b9f7adb112052c40a58337f1b37b01
Armijn Hemel <armijn@tjaldur.nl>
2022-04-02 20:53:14 +0200

    android_sparse_data: fix import

--- src/parsers/firmware/android_sparse_data/UnpackParser.py
+++ src/parsers/firmware/android_sparse_data/UnpackParser.py
@@ -1,7 +1,7 @@
 
 import os
 from UnpackParser import WrappedUnpackParser
-from bangandroid import unpack_android_sparse_data
+from bangunpack import unpack_android_sparse_data
 
 class AndroidSparseDataUnpackParser(WrappedUnpackParser):
     extensions = ['.new.dat', 'new.dat.br']
