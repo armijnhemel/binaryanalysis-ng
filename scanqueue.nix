a924649801b1efbfc45fa2f9865bcd3cb8158c2f
Armijn Hemel <armijn@tjaldur.nl>
2022-04-05 14:40:02 +0200

    add Nix file for scanqueue

---
+++ scanqueue.nix
@@ -0,0 +1,24 @@
+let
+  # Use `niv update` to update nixpkgs.
+  # See https://github.com/nmattia/niv/
+  sources = import ./nix/sources.nix;
+
+  pkgs = import sources.nixpkgs { config.allowUnfree = true; overlays = []; };
+
+  my-python = pkgs.python39.withPackages (p: with p; [
+    celery
+    click
+    flask
+    gevent
+    gunicorn
+    pyyaml
+    redis
+  ]);
+    
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    my-python
+    redis
+  ];
+}
