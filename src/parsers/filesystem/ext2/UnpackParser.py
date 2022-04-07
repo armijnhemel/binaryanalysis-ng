3e50501f477d95edd799328e243f424c75c03565
Armijn Hemel <armijn@tjaldur.nl>
2022-04-07 23:44:00 +0200

    ext2: start on rewriting parser using kaitai struct

--- src/parsers/filesystem/ext2/UnpackParser.py
+++ src/parsers/filesystem/ext2/UnpackParser.py
@@ -21,9 +21,12 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 
+import math
 import os
 import shutil
 import subprocess
+import tempfile
+import uuid
 
 from UnpackParser import UnpackParser, check_condition
 from UnpackParserException import UnpackParserException
@@ -31,7 +34,10 @@ from UnpackParserException import UnpackParserException
 from UnpackParser import WrappedUnpackParser
 from bangfilesystems import unpack_ext2
 
+from . import ext2
+
 class Ext2UnpackParser(WrappedUnpackParser):
+#class Ext2UnpackParser(UnpackParser):
     extensions = []
     signatures = [
         (0x438,  b'\x53\xef')
@@ -45,7 +51,34 @@ class Ext2UnpackParser(WrappedUnpackParser):
         check_condition(shutil.which('e2ls') is not None, "e2ls program not found")
         check_condition(shutil.which('e2cp') is not None, "e2cp program not found")
 
+        self.infile.seek(1024)
+
         # parse the superblock
+        try:
+            self.superblock = ext2.Ext2.SuperBlockStruct.from_io(self.infile)
+        except (Exception, ValidationFailedError) as e:
+            raise UnpackParserException(e.args)
+
+        check_condition(self.superblock.block_size * self.superblock.blocks_count <= self.fileresult.filesize,
+                        "declared file system size larger than file size")
+
+        # does this have to be math.ceil()?
+        block_groups = math.ceil(self.superblock.blocks_count/self.superblock.blocks_per_group)
+        self.unpacked_size = self.superblock.block_size * self.superblock.blocks_count
+
+        # extract a volume name if present
+        try:
+            self.volume_name = self.superblock.volume_name.decode()
+        except:
+            self.volume_name = ""
+
+        # extract a last mounted path if present
+        try:
+            self.last_mounted = self.superblock.last_mounted.decode()
+        except:
+            self.last_mounted = ""
+
+        self.fs_uuid = uuid.UUID(bytes=self.superblock.uuid)
 
     # no need to carve from the file
     def carve(self):
