1afad0394d9c37bbba37b51faeedf8520d75b493
Armijn Hemel <armijn@tjaldur.nl>
2022-07-23 22:00:33 +0200

    nix: drop python 3.9

--- analysis.nix
+++ analysis.nix
@@ -5,7 +5,7 @@ let
 
   pkgs = import sources.nixpkgs { config.allowUnfree = true; overlays = []; };
 
-  my-python = pkgs.python39.withPackages (p: with p; [
+  my-python = pkgs.python3.withPackages (p: with p; [
     deepdiff
     defusedxml
     dockerfile-parse
