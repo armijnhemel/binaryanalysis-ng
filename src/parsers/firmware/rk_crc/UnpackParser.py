e3ae62f8d2a330246f64ac7ab1140a332eddde04
Armijn Hemel <armijn@tjaldur.nl>
2022-10-15 17:50:37 +0200

    rk_crc: extra sanity check

--- src/parsers/firmware/rk_crc/UnpackParser.py
+++ src/parsers/firmware/rk_crc/UnpackParser.py
@@ -42,6 +42,7 @@ class RkCrcUnpackParser(UnpackParser):
             self.data = rk_crc.RkCrc.from_io(self.infile)
         except (Exception, ValidationFailedError) as e:
             raise UnpackParserException(e.args)
+        check_condition(len(self.data.data) > 0, "empty data section")
 
     def unpack(self):
         unpacked_files = []
