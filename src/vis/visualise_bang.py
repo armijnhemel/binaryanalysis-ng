6c4b3bca0d6df80d21f7f82161d53e477a101c33
Armijn Hemel <armijn@tjaldur.nl>
2023-01-30 20:51:17 +0100

    vis: fix help message

--- src/vis/visualise_bang.py
+++ src/vis/visualise_bang.py
@@ -23,7 +23,7 @@ import click
 import pydot
 
 
-@click.command(short_help='process BANG result files and output ELF graphs')
+@click.command(short_help='process BANG result files and output graphviz files')
 @click.option('--config-file', '-c', required=True, help='configuration file', type=click.File('r'))
 @click.option('--directory', '-d', 'result_directory', required=True, help='BANG result directory', type=click.Path(exists=True))
 def main(config_file, result_directory):
