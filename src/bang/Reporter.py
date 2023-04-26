bbaacf501a6669dbba61f0240d8c71b3216f159b
Armijn Hemel <armijn@tjaldur.nl>
2023-04-26 19:00:56 +0200

    start on reporter plugins

---
+++ src/bang/Reporter.py
@@ -0,0 +1,37 @@
+# Binary Analysis Next Generation (BANG!)
+#
+# This file is part of BANG.
+#
+# BANG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# BANG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License, version 3, along with BANG.  If not, see
+# <http://www.gnu.org/licenses/>
+#
+# Licensed under the terms of the GNU Affero General Public License
+# version 3
+# SPDX-License-Identifier: AGPL-3.0-only
+
+import rich
+import rich.console
+import rich.markdown
+import rich.pretty
+import rich.table
+
+
+class Reporter():
+    def __init__(self):
+        pass
+
+    '''Override any methods'''
+    def create_report(self, md):
+        '''Create a report, returns a title and pretty printed
+        representation of data'''
+        return None, None
