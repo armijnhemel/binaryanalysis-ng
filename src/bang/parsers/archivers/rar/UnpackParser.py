a5342dec4422e35a92bbbcfd01959393fc2655f1
Armijn Hemel <armijn@tjaldur.nl>
2023-05-02 21:00:37 +0200

    rar: add correct license text, import pathlib

--- src/bang/parsers/archivers/rar/UnpackParser.py
+++ src/bang/parsers/archivers/rar/UnpackParser.py
@@ -1,11 +1,35 @@
+# Binary Analysis Next Generation (BANG!)
+#
+# This file is part of BANG.
+#
+# BANG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# BANG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License, version 3, along with BANG.  If not, see
+# <http://www.gnu.org/licenses/>
+#
+# Copyright Armijn Hemel
+# Licensed under the terms of the GNU Affero General Public License
+# version 3
+# SPDX-License-Identifier: AGPL-3.0-only
+
 import os
+import pathlib
 import re
 import subprocess
-from . import rar
 
 from bang.UnpackParser import UnpackParser
 from bang.UnpackParserException import UnpackParserException
 from kaitaistruct import ValidationFailedError
+
+from . import rar
 
 
 class RarUnpackParser(UnpackParser):
